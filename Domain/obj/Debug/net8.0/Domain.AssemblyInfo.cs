9b895f700f437edab5b39e19e92418b926b6f777
nbs282 <Norapala462@gmail.com>
2025-04-21 17:49:22 -0300

    Add partial implementation of admin navbar

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69e097c59182a0368bcda5a20e3237455010a51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aada6646e3be04a23d2b9d2957e6ec18f9be9d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
