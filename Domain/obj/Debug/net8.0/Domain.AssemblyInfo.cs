41d2e04855105f3556dc77b419f2437bc6972669
nbs282 <Norapala462@gmail.com>
2025-04-22 19:26:22 -0300

    change the navlinks of admin system

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b895f700f437edab5b39e19e92418b926b6f777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c817448d803bafd5e7aa0ab894eb0d6cdd404af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
