f89a8d15c910521e6e89dfa9dd10bd071f1841c6
nbs282 <Norapala462@gmail.com>
2025-04-23 00:44:11 -0300

    Implement resource management: added listing, editing, and deletion functionality in a single page

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b895f700f437edab5b39e19e92418b926b6f777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7747af8e34a9de067855238303fc20695b1be09a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
