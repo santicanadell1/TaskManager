a6771cd9418454b7b42312f231e859bb4615e55e
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-23 19:03:06 -0300

    Red: create user with empty email

--- Domain.Test/UserTest.cs
+++ Domain.Test/UserTest.cs
@@ -27,13 +27,23 @@ public class UserTest
     }
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
-    public void NewUser_WhenLasttNameIsNull_ThenThrowArgumentNullException()
+    public void NewUser_WhenLastNameIsNull_ThenThrowArgumentNullException()
     {
         //arrange
         User user ;
         DateTime birthday = DateTime.Parse("10/05/2005");
         //act
         user = new User("First Name", "", "Email", birthday, "Password");
+    }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NewUser_WhenEmailIsNull_ThenThrowArgumentNullException()
+    {
+        //arrange
+        User user ;
+        DateTime birthday = DateTime.Parse("10/05/2005");
+        //act
+        user = new User("First Name", "Last Name", "", birthday, "Password");
     }
     
 }
--- Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
+++ Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2385b4801a0455504894b1e57df40b36c5e3aa12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cca7032b8dd4d17a1a165106594679abd5cdba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
