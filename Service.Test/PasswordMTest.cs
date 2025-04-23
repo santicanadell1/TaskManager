ab1bf6dddc525425650d23958d89b740386dfeb6
nbs282 <Norapala462@gmail.com>
2025-04-23 20:27:54 -0300

    Red: hash password not implemented

--- Service.Test/PasswordMTest.cs
+++ Service.Test/PasswordMTest.cs
@@ -13,4 +13,20 @@ public class PasswordMTest
         // Assert
         Assert.IsTrue(isValid, "The default password should be valid.");
     }
+    
+    [TestMethod]
+    public void HashPassword_ShouldReturnSameHashForSamePassword()
+    {
+        // Arrange
+        string password1 = "FNSabc1?";
+        string password2 = "FNSabc1?";
+
+        // Act
+        string hash1 = PasswordManager.HashPassword(password1);
+        string hash2 = PasswordManager.HashPassword(password2);
+
+        // Assert
+        Assert.AreEqual(hash1, hash2, "The hash should be the same for the same password.");
+    }
+    
 }
