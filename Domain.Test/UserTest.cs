9b751872a850c0da03b2538ef22253cc2279d7b5
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-23 18:23:33 -0300

    Red: user constructor is not implemented

---
+++ Domain.Test/UserTest.cs
@@ -0,0 +1,17 @@
+namespace Domain.Test;
+
+[TestClass]
+public class UserTest
+{
+    [TestMethod]
+    public void NewUser_WhenConstructorIsNotEmpty_ThenUserIsCreated()
+    {
+        // Arrange
+        User user;
+        DateTime birthday = DateTime.Parse("10/05/2005");
+        //act
+        user = new User("First Name", "Last Name", "Email", birthday);
+        //assert
+        Assert.IsNotNull(user);
+    }
+}
