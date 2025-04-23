c7adce4d4695f095bfc06e072afedd2ccd27394c
Nicolas Bidenti <NB305108@fi365.ort.edu.uy>
2025-04-23 09:16:00 -0300

    add resource in modal task

--- Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
+++ Domain.Test/obj/Debug/net8.0/Domain.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8c73f26c0cf0298b8a49b914cf3cc644b73e58f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a17a3fbad476a015cee3855529e331b2795affa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
