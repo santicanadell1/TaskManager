736f9b2af26b7a711a2cc523bb96d1e4b40fa35f
nbs282 <Norapala462@gmail.com>
2025-04-23 19:34:41 -0300

    Red: Password Manager constructor is not implemented

---
+++ Service.Test/PasswordMTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Service.Test;
+
+[TestClass]
+public class PasswordMTest
+{
+    [TestMethod]
+    public void DefaultPassword_ShouldBeValid()
+    {
+        // Arrange
+        PasswordManager passwordManager = new PasswordManager();  // default password is set in the constructor
+
+        // Act
+        bool isValid = passwordManager.IsValidPassword(passwordManager.Password);
+
+        // Assert
+        Assert.IsTrue(isValid, "The default password should be valid.");
+    }
+}
--- Service.Test/obj/Debug/net8.0/Service.Test.AssemblyInfo.cs
+++ Service.Test/obj/Debug/net8.0/Service.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7747af8e34a9de067855238303fc20695b1be09a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ea030e7e14b147331f822e144dc8d4296f62ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
