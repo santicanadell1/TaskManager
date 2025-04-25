7299b7a328af8f29f1369111938bdf2b744e4ee6
nbs282 <Norapala462@gmail.com>
2025-04-25 19:54:02 -0300

    solving problem with an directory

--- Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
+++ Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc8e8fd4b942d420a89d631a437c1f7283603c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb594151f8cad9223499147d4e81686317c923a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
