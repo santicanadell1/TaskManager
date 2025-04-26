529a6f7cd65037f618eff871da236d502c1c8dcd
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-26 14:56:18 -0300

    Red: Task's name cannot be empty or white spaces

--- Domain.Test/TaskTest.cs
+++ Domain.Test/TaskTest.cs
@@ -17,4 +17,16 @@ public class TaskTests
         //Assert
         Assert.IsNotNull(task);
     }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void NewTask_WhenTitleIsEmptyOrWhiteSpaces_ThenThrowArgumentException()
+    {
+        //Arrange
+        Task task;
+        DateTime startDate = DateTime.Now;
+        DateTime endDate = DateTime.Today;
+        List<Task> previousTasks = new List<Task>();
+        //Act
+        task = new Task("", "Description", startDate, endDate , 1, previousTasks);
+    }
 }
