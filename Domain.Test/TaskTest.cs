51b5c5ad20f409edbb854bd876221d5117a8cd4a
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-26 14:50:06 -0300

    Red: Task can't be created with parameters

---
+++ Domain.Test/TaskTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Test;
+using Domain;
+
+[TestClass]
+public class TaskTests
+{
+    [TestMethod]
+    public void NewTask_WhenConstructorIsNotEmpty_ThenTaskIsNotNull()
+    {
+        //Arrange
+        Task task;
+        DateTime startDate = DateTime.Now;
+        DateTime endDate = DateTime.Today;
+        List<Task> previousTasks = new List<Task>();
+        //Act
+        task = new Task("Title", "Description", startDate, endDate ,1, previousTasks);
+        //Assert
+        Assert.IsNotNull(task);
+    }
+}
