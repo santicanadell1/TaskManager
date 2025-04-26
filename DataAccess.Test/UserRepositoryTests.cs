cebc1c306c809428d58599daf1f1979a2cfd9269
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-26 15:54:25 -0300

    Red: When add a new user the list must contain it

--- DataAccess.Test/UserRepositoryTests.cs
+++ DataAccess.Test/UserRepositoryTests.cs
@@ -15,6 +15,16 @@ public class UserRepositoryTests
         // Assert
         Assert.IsNotNull(userRepository);
     }
-    
+    [TestMethod]
+    public void AddNewUser_WhenAddNewUser_ListShouldContainUser()
+    {
+        // Arrange
+        UserRepository userRepository = new UserRepository();
+        User user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Parse("20/12/12"), "Password");
+        // Act
+        userRepository.addUser(user);
+        // Assert
+        userRepository.getAll.contains(user);
+    }
     
 }
