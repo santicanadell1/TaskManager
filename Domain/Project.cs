ac67a073a4ba41a129448a1d3edbc950d9c6ed4d
nbs282 <Norapala462@gmail.com>
2025-04-26 16:46:37 -0300

    Green: the project name can not be empty, throw a exception

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -8,8 +8,18 @@ public class Project
     public List<User> members;
     public List<Task> tasks;
     public User adminProject;
-    public string Name{get;set;}
-    public string Description{get;set;}
+    public string Name
+    {
+        get => name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Project name cannot be null, empty, or whitespace.");
+            }
+            name = value;
+        }
+    }    public string Description{get;set;}
     public DateTime StartDate{get;set;}
     public List<User> Members{get;set;}
     public List<Task> Tasks{get;set;}
