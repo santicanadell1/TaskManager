c78347a4abaeaa89c6031871cbea5a4e7a8aeee4
SantiCanadell <santicanadell0@gmail.com>
2025-04-26 16:06:11 -0300

    Green, when a description is empty throws exception

--- Domain/Notification.cs
+++ Domain/Notification.cs
@@ -6,7 +6,18 @@ public class Notification
     public string description;
     
     public bool Read { get => read; set => read = value; }
-    public string Description { get => description; set => description = value; }
+
+    public string Description
+    {
+        get => description;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Description cannot be null or empty.");
+            }
+        }
+    }
 
     public Notification(bool read, string description)
     {
