c24e026e84a5cdebc04a7fcf8df34c805964e278
nbs282 <Norapala462@gmail.com>
2025-04-26 16:00:26 -0300

    Green: the atribute description is implemented

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -3,7 +3,8 @@ namespace Domain;
 public class Project
 {
     public string name;
+    public string description;
     public string Name{get;set;}
-
+    public string Description{get;set;}
     public Project(){}
 }
