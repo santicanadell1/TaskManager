4a21ef37ec3f16ec22a02f096d87ff8d4225001d
FeliD <FS281987@fi365.ort.edu.uy>
2025-04-26 15:44:35 -0300

    Red: When repository is created should not be null

---
+++ DataAccess.Test/UserRepositoryTests.cs
@@ -0,0 +1,12 @@
+namespace DataAccess.Test;
+using DataAccess;
+
+[TestClass]
+public class UserRepositoryTests
+{
+    [TestMethod]
+    public void NewUserRepository_WhenRepositoryIsCreated_ShouldNotBeNull()
+    {
+        throw new NotImplementedException();
+    }
+}
