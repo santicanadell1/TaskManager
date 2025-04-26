379f9e0efbe163e8c10c9c33f1223abe2283bf9f
nbs282 <Norapala462@gmail.com>
2025-04-26 16:05:31 -0300

    Green: the atribute startDate, and set/get is implemented

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -4,7 +4,9 @@ public class Project
 {
     public string name;
     public string description;
+    public DateTime startDate;
     public string Name{get;set;}
     public string Description{get;set;}
+    public DateTime StartDate{get;set;}
     public Project(){}
 }
