90df795d3b89377326ff11e855fd7b639b1c4002
nbs282 <Norapala462@gmail.com>
2025-04-26 16:09:06 -0300

    Green: the atribute memebers, and set/get is implemented

--- Domain/Project.cs
+++ Domain/Project.cs
@@ -5,8 +5,10 @@ public class Project
     public string name;
     public string description;
     public DateTime startDate;
+    public List<User> members;
     public string Name{get;set;}
     public string Description{get;set;}
     public DateTime StartDate{get;set;}
+    public List<User> Members{get;set;}
     public Project(){}
 }
