2ed8c77dd923211967e4b36a97ea0fd046f5fe0e
nbs282 <Norapala462@gmail.com>
2025-04-26 22:23:09 -0300

    Red: userDto Email is not implemented

--- Service.Test/ModelsTests/UserDTOTest.cs
+++ Service.Test/ModelsTests/UserDTOTest.cs
@@ -24,4 +24,14 @@ public class UserDTOTest
         // act & assert
         Assert.IsNull(user.LastName);
     }
+    
+    [TestMethod]
+    public void NewUser_WhenEmailIsNull_ThenUserIsNotCreated()
+    {
+        // arrange
+        UserDTO user = new UserDTO { Email = null };
+
+        // act & assert
+        Assert.IsNull(user.Email);
+    }
 }
