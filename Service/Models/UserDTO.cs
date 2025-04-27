d574411331c081232befcca5ad725e241643e77d
nbs282 <Norapala462@gmail.com>
2025-04-26 22:27:31 -0300

    Green: userDto Password is implemented

--- Service/Models/UserDTO.cs
+++ Service/Models/UserDTO.cs
@@ -15,5 +15,8 @@ public class UserDTO
     
     [Required(ErrorMessage = "Birthday is required.")]
     public DateTime Birthday { get; set; }
+    
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
 
 }
