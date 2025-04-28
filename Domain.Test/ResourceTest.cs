fcf7b35acfc36dcee626e9ef8c512d9e4a7a034f
nbs282 <Norapala462@gmail.com>
2025-04-28 15:33:10 -0300

    Refactor: Refactor tests to use custom exceptions for resourcesvalidation

--- Domain.Test/ResourceTest.cs
+++ Domain.Test/ResourceTest.cs
@@ -1,62 +1,58 @@
-namespace Domain.Test;
+using Domain.Exceptions;
 
-[TestClass]
-public class ResourceTest
+namespace Domain.Test
 {
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewResource_WhenNameIsEmpty_ThenThrowException()
+    [TestClass]
+    public class ResourceTest
     {
-        //Arrange
-        Resource res;
-        //Act
-        res = new Resource("","type","description");
-        
-      
+        [TestMethod]
+        [ExpectedException(typeof(ResourceNameException))]
+        public void NewResource_WhenNameIsEmpty_ThenThrowResourceNameException()
+        {
+            // Arrange
+            Resource res;
+            // Act
+            res = new Resource("", "type", "description");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ResourceTypeException))]
+        public void NewResource_WhenTypeIsEmpty_ThenThrowResourceTypeException()
+        {
+            // Arrange
+            Resource res;
+            // Act
+            res = new Resource("name", "", "description");
+        }
+
+        [TestMethod]
+        public void NewResource_WithValidValues_ShouldCreateResourceCorrectly()
+        {
+            // Arrange
+            var name = "Laptop";
+            var type = "Hardware";
+            var description = "Dell Latitude";
+
+            // Act
+            var res = new Resource(name, type, description);
+
+            // Assert
+            Assert.AreEqual(name, res.Name);
+            Assert.AreEqual(type, res.Type);
+            Assert.AreEqual(description, res.Description);
+        }
+
+        [TestMethod]
+        public void UpdateResourceName_ShouldChangeNameSuccessfully()
+        {
+            // Arrange
+            var res = new Resource("Old Name", "Humano", "Some description");
+
+            // Act
+            res.Name = "New Name";
+
+            // Assert
+            Assert.AreEqual("New Name", res.Name);
+        }
     }
-    
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewResource_WhenTypeIsEmpty_ThenThrowException()
-    {
-        //Arrange
-        Resource res;
-        //Act
-        res = new Resource("name","","description");
-        
-        
-    }
-    
-    [TestMethod]
-    public void NewResource_WithValidValues_ShouldCreateResourceCorrectly()
-    {
-        // Arrange
-        var name = "Laptop";
-        var type = "Hardware";
-        var description = "Dell Latitude";
-
-        // Act
-        var res = new Resource(name, type, description);
-
-        // Assert
-        Assert.AreEqual(name, res.Name);
-        Assert.AreEqual(type, res.Type);
-        Assert.AreEqual(description, res.Description);
-    }
-    
-    [TestMethod]
-    public void UpdateResourceName_ShouldChangeNameSuccessfully()
-    {
-        // Arrange
-        var res = new Resource("Old Name", "Humano", "Some description");
-
-        // Act
-        res.Name = "New Name";
-
-        // Assert
-        Assert.AreEqual("New Name", res.Name);
-    }
-
-
-    
 }
--- Domain/Resource.cs
+++ Domain/Resource.cs
@@ -1,28 +1,38 @@
-﻿namespace Domain;
+﻿using Domain.Exceptions;
 
-public class Resource
+namespace Domain
 {
-    public string name;
-    public string type;
-    public string description;
-    
-    
-    public string Name {get => name;
-                        set {name = string.IsNullOrEmpty(value) ? throw new ArgumentException("the resource name can not be empty"): value; }
-    }
-    public string Type {get => type ;
-        set { type = string.IsNullOrEmpty(value) ? throw new ArgumentException("the resource type can not be empty"): value; }
-    }
-    public string Description {get; set;}
-
-    
-
-    public Resource(string name, string type, string description)
+    public class Resource
     {
-        this.Name = name;
-        this.Type = type;
-        this.Description = description;
+        public string name;
+        public string type;
+        public string description;
+
+        public string Name
+        {
+            get => name;
+            set 
+            {
+                name = string.IsNullOrEmpty(value) ? throw new ResourceNameException() : value;
+            }
+        }
+
+        public string Type
+        {
+            get => type;
+            set
+            {
+                type = string.IsNullOrEmpty(value) ? throw new ResourceTypeException() : value;
+            }
+        }
+
+        public string Description { get; set; }
+
+        public Resource(string name, string type, string description)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Description = description;
+        }
     }
 }
-
-    
