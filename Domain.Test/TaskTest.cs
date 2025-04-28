b8610e9f49082d72af0c20a3d53886e633c91c5f
nbs282 <Norapala462@gmail.com>
2025-04-28 15:55:05 -0300

    Refactor: Refactor tests to use custom exceptions for tasks validations

--- Domain.Test/TaskTest.cs
+++ Domain.Test/TaskTest.cs
@@ -1,68 +1,80 @@
-﻿namespace Domain.Test;
-using Domain;
+﻿using Domain.Exceptions;
 
-[TestClass]
-public class TaskTests
+namespace Domain.Test
 {
-    [TestMethod]
-    public void NewTask_WhenConstructorIsNotEmpty_ThenTaskIsNotNull()
+    [TestClass]
+    public class TaskTests
     {
-        //Arrange
-        Task task;
-        DateTime startDate = DateTime.Now;
-        DateTime endDate = DateTime.Today;
-        List<Task> previousTasks = new List<Task>();
-        //Act
-        task = new Task("Title", "Description", startDate, endDate ,1, previousTasks);
-        //Assert
-        Assert.IsNotNull(task);
-    }
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewTask_WhenTitleIsEmptyOrWhiteSpaces_ThenThrowArgumentException()
-    {
-        //Arrange
-        Task task;
-        DateTime startDate = DateTime.Now;
-        DateTime endDate = DateTime.Today;
-        List<Task> previousTasks = new List<Task>();
-        //Act
-        task = new Task("", "Description", startDate, endDate , 1, previousTasks);
-    }
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewTask_WhenDescriptionIsEmptyOrWhiteSpaces_ThenThrowArgumentException()
-    {
-        //Arrange
-        Task task;
-        DateTime startDate = DateTime.Now;
-        DateTime endDate = DateTime.Today;
-        List<Task> previousTasks = new List<Task>();
-        //Act
-        task = new Task("Title", "", startDate,  endDate ,1, previousTasks);
-    }
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewTask_WhenDurationIsZeroOrNegative_ThenThrowArgumentException()
-    {
-        //Arrange
-        Task task;
-        DateTime startDate = DateTime.Now;
-        DateTime endDate = DateTime.Today;
-        List<Task> previousTasks = new List<Task>();
-        //Act
-        task = new Task("Title", "Description", startDate,  endDate ,-11, previousTasks);
-    }
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentException))]
-    public void NewTask_WhenExpectedEndDateIsBeforeExpectedStartDate_ThenThrowArgumentException()
-    {
-        //Arrange
-        Task task;
-        DateTime startDate = DateTime.Parse("2023-01-01");
-        DateTime endDate = DateTime.Parse("2022-01-01");
-        List<Task> previousTasks = new List<Task>();
-        //Act
-        task = new Task("Title", "Description", startDate,  endDate ,1, previousTasks);
+        [TestMethod]
+        public void NewTask_WhenConstructorIsNotEmpty_ThenTaskIsNotNull()
+        {
+            // Arrange
+            Task task;
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Parse("2026-09-01");
+            List<Task> previousTasks = new List<Task>();
+
+            // Act
+            task = new Task("Title", "Description", startDate, endDate, 1, previousTasks);
+
+            // Assert
+            Assert.IsNotNull(task);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TaskTitleException))]
+        public void NewTask_WhenTitleIsEmptyOrWhiteSpaces_ThenThrowTaskTitleException()
+        {
+            // Arrange
+            Task task;
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Today;
+            List<Task> previousTasks = new List<Task>();
+
+            // Act
+            task = new Task("", "Description", startDate, endDate, 1, previousTasks);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TaskDescriptionException))]
+        public void NewTask_WhenDescriptionIsEmptyOrWhiteSpaces_ThenThrowTaskDescriptionException()
+        {
+            // Arrange
+            Task task;
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Today;
+            List<Task> previousTasks = new List<Task>();
+
+            // Act
+            task = new Task("Title", "", startDate, endDate, 1, previousTasks);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TaskDurationException))]
+        public void NewTask_WhenDurationIsZeroOrNegative_ThenThrowTaskDurationException()
+        {
+            // Arrange
+            Task task;
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Parse("2026-09-01");
+            List<Task> previousTasks = new List<Task>();
+
+            // Act
+            task = new Task("Title", "Description", startDate, endDate, -11, previousTasks); // Should throw exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(TaskEndDateException))]
+        public void NewTask_WhenExpectedEndDateIsBeforeExpectedStartDate_ThenThrowTaskEndDateException()
+        {
+            // Arrange
+            Task task;
+            DateTime startDate = DateTime.Parse("2023-01-01");
+            DateTime endDate = DateTime.Parse("2022-01-01");
+            List<Task> previousTasks = new List<Task>();
+
+            // Act
+            task = new Task("Title", "Description", startDate, endDate, 1, previousTasks); // Should throw exception
+        }
     }
 }
