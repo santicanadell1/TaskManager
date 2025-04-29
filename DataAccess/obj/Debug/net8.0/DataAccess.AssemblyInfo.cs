deddbc52d05562f49926b285e33884ceb020acb2
nbs282 <Norapala462@gmail.com>
2025-04-28 21:20:42 -0300

    merge login-service into develop

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b729f3659e48ec9336829e760acf9b4b5600c378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+184306d53f1c3ff46c96ec06373a3f645b4fa232")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
