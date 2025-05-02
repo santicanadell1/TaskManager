59ed945ba9dc252c30c43a482488c14c473a311c
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-02 19:06:11 -0300

    Green: When adding a new project to the project repository, projectRepository's list contains it

--- DataAccess/ProjectRepository.cs
+++ DataAccess/ProjectRepository.cs
@@ -4,10 +4,15 @@ namespace DataAccess;
 
 public class ProjectRepository
 {
-    private List<Project> projects;
+    private List<Project> _projects;
     public ProjectRepository()
     {
-        projects = new List<Project>();
+        Projects = new List<Project>();
     }
+    public List<Project> Projects { get; set; }
     
+    public void AddProject(Project project)
+    {
+        Projects.Add(project);
+    }
 }
