9651e0070ada61c1a8344321cd7c972185b2775c
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-02 18:52:32 -0300

    Red: When creating a project repository it should not be null

---
+++ DataAccess.Tests/ProjectRepositoryTest.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccess.Test;
+
+[TestClass]
+public class ProjectRepositoryTest
+{
+    [TestMethod]
+    public void NewProjectRepository_WhenRepositoryIsCreated_ShouldNotBeNull()
+    {
+        throw new NotImplementedException();
+    }
+}
