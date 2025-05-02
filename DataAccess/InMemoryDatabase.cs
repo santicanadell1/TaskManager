9feda1f1dfc35946382ce7afc23d16004190f493
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-02 20:15:51 -0300

    Adding projectRepository to inMemoryDatabase

--- DataAccess/InMemoryDatabase.cs
+++ DataAccess/InMemoryDatabase.cs
@@ -6,10 +6,12 @@ public class InMemoryDatabase
 {
     public UserRepository users;
     public NotificationRepository notifications;
+    public ProjectRepository projects;
 
     public InMemoryDatabase()
     {
         users = new UserRepository();
         notifications = new NotificationRepository();
+        projects = new ProjectRepository();
     }
 }
