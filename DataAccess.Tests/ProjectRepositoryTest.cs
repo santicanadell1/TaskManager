a8fa6dbe3dcb4ab3dd9eac03952c88622940f47c
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-02 19:19:25 -0300

    Red: When getting a user from the repository it should be equal to the original one

--- DataAccess.Tests/ProjectRepositoryTest.cs
+++ DataAccess.Tests/ProjectRepositoryTest.cs
@@ -22,4 +22,19 @@ public class ProjectRepositoryTest
         projectRepository.AddProject(project);
         Assert.IsTrue(projectRepository.GetAllProjects().Contains(project));
     }
+    
+    [TestMethod]
+    public void AddNewProject_WhenGettingUser_ShouldReturnUser()
+    {
+        ProjectRepository projectRepository;
+        projectRepository = new ProjectRepository();
+        Project project = new Project();
+        project.Name = "Project 1";
+        Project project2 = new Project();
+        project2.Name = "Project 2";
+        projectRepository.AddProject(project);
+        projectRepository.AddProject(project2);
+        Assert.AreEqual(projectRepository.GetProject("Project 2"), project2);
+
+    }
 }
