fe0b8fc62e5ca9cf05538d7ee343b829f9f18c29
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-02 20:42:15 -0300

    Red: When adding a new user, if its name already exists should throw an exception

---
+++ DataAccess/UserRepositoryExceptions/UserNameIsDuplicatedException.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Exceptions.UserRepositoryExceptions;
+
+public class UserNameIsDuplicatedException : Exception
+{
+    public UserNameIsDuplicatedException() : base("The user name already exists."){}
+}
