dbffab6105615d27e158adb7e8ef2208460c3a3b
nbs282 <Norapala462@gmail.com>
2025-05-03 18:23:37 -0300

    Red: addResource and get are not implemented

--- DataAccess.Tests/ResourceRepositoryTests.cs
+++ DataAccess.Tests/ResourceRepositoryTests.cs
@@ -1,3 +1,5 @@
+using Domain;
+
 namespace DataAccess.Test;
 
 [TestClass]
@@ -15,6 +17,18 @@ public class ResourceRepositoryTests
     {
         var resources = _resourceRepository.GetAll();
         Assert.AreEqual(0, resources.Count);
+    }
+    
+    [TestMethod]
+    public void AddResource_ShouldAddResource_WhenValidResourceIsProvided()
+    {
+        var resource = new Resource("Resource1", "TypeA", "Description of Resource1");
+
+        _resourceRepository.AddResource(resource);
+
+        var addedResource = _resourceRepository.Get(r => r.Name == resource.Name);
+        Assert.IsNotNull(addedResource);
+        Assert.AreEqual(resource.Name, addedResource?.Name);
     }
     
 }
