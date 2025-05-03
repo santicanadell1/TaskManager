6c0fa22f16038a78c77df4cb8b64d4673b63a520
nbs282 <Norapala462@gmail.com>
2025-05-03 20:27:14 -0300

    Red: the atribute Type is not implemented

--- Service.Test/ModelsTests/ResourceDTOTest.cs
+++ Service.Test/ModelsTests/ResourceDTOTest.cs
@@ -22,4 +22,22 @@ public class ResourceDTOTest
         Assert.AreEqual(1, validationResults.Count);  
         Assert.AreEqual("Name is required.", validationResults[0].ErrorMessage);  
     }
+    
+    [TestMethod]
+    public void ResourceDTO_ShouldFailValidation_WhenTypeIsNull()
+    {
+        var resourceDTO = new ResourceDTO
+        {
+            Name = "Resource1",
+            Type = null,  
+        };
+
+        var validationResults = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(resourceDTO, new ValidationContext(resourceDTO), validationResults, true);
+
+        Assert.IsFalse(isValid);  
+        Assert.AreEqual(1, validationResults.Count);  
+        Assert.AreEqual("LastName is required.", validationResults[0].ErrorMessage);  
+    }
+
 }
