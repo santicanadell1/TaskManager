481569da1139296cc619409746f80719aec568d4
nbs282 <Norapala462@gmail.com>
2025-05-03 20:27:41 -0300

    Red: the atribute Type is implemented

--- Service/Models/ResourceDTO.cs
+++ Service/Models/ResourceDTO.cs
@@ -6,4 +6,7 @@ public class ResourceDTO
 {
     [Required(ErrorMessage = "Name is required.")]
     public string Name { get; set; }
+    
+    [Required(ErrorMessage = "LastName is required.")]
+    public string Type { get; set; }
 }
