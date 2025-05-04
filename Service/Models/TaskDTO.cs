8961a4921ade20fe0803f108d0d2ede85f326e7e
SantiCanadell <santicanadell0@gmail.com>
2025-05-03 23:43:08 -0300

    green: when StartDate is valid, task is created

--- Service/Models/TaskDTO.cs
+++ Service/Models/TaskDTO.cs
@@ -13,5 +13,6 @@ public class TaskDTO
     [Required(ErrorMessage = "ExpectedStartDate is required.")]
     public DateTime ExpectedStartDate { get; set; }
     
-    
+    [Required(ErrorMessage = "StartDate is required.")]
+    public DateTime StartDate { get; set; }
 }
