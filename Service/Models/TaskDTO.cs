05ff9cc3df7adc3c594d2586874e6da6d1fe4fbc
SantiCanadell <santicanadell0@gmail.com>
2025-05-03 23:33:49 -0300

    green: when description is null the task is not created

--- Service/Models/TaskDTO.cs
+++ Service/Models/TaskDTO.cs
@@ -7,5 +7,6 @@ public class TaskDTO
     [Required(ErrorMessage = "Title is required.")]
     public string Title { get; set; }
     
-    
+    [Required(ErrorMessage = "Description is required.")]
+    public string Description { get; set; }
 }
