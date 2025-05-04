dc35730dcec51b142b7dc976eb9c47c85b1155c2
Santi <santicanadell0@gmail.com>
2025-05-04 18:29:07 -0300

    red: TestClass TaskServce

---
+++ Service.Test/TaskServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using Domain;
+using Service.Models;
+using Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Exceptions;
+
+namespace Service.Test
+{
+    [TestClass]
+    public class TaskServiceTest
+    {
+        private InMemoryDatabase _database;
+        private TaskService _taskService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _database = new InMemoryDatabase();
+            _taskService = new TaskService(_database);
+        }
