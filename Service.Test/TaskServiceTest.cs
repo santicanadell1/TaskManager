89daf42019a7b27ab8710951afa2e32376e1881b
Santi <santicanadell0@gmail.com>
2025-05-04 20:09:19 -0300

    red: ToEntity_ShouldConvertTaskDTOToTaskCorrectly not working

--- Service.Test/TaskServiceTest.cs
+++ Service.Test/TaskServiceTest.cs
@@ -16,8 +16,8 @@ namespace Service.Test
         [TestInitialize]
         public void Setup()
         {
-            _database = new InMemoryDatabase();  
-            _taskService = new TaskService(_database); 
+            _database = new InMemoryDatabase();
+            _taskService = new TaskService(_database);
         }
 
 
@@ -25,89 +25,90 @@ namespace Service.Test
         public void CalculateEarlyStart_ShouldReturnCorrectDate_WhenNoPreviousTasks()
         {
             var task = new Task(
-                "Task 1",                              
-                "Description of Task 1",               
-                new DateTime(2025, 5, 1),              
-                5,                                     
-                new List<Task>(),               
-                new List<Task>()               
+                "Task 1",
+                "Description of Task 1",
+                new DateTime(2025, 5, 1),
+                5,
+                new List<Task>(),
+                new List<Task>()
             );
 
             DateTime earlyStart = _taskService.CalculateEarlyStart(task);
 
-       
+
             Assert.AreEqual(new DateTime(2025, 5, 1), earlyStart);
         }
+
         [TestMethod]
         public void CalculateEarlyFinish_ShouldReturnCorrectDate_WhenGivenDuration()
         {
             var task = new Task(
-                "Task 1",                             
-                "Description of Task 1",              
-                new DateTime(2025, 5, 1),             
-                5,                                     
-                new List<Task>(),              
-                new List<Task>()             
+                "Task 1",
+                "Description of Task 1",
+                new DateTime(2025, 5, 1),
+                5,
+                new List<Task>(),
+                new List<Task>()
             );
 
             DateTime earlyFinish = _taskService.CalculateEarlyFinish(task);
 
-       
-            Assert.AreEqual(new DateTime(2025, 5, 6), earlyFinish); 
+
+            Assert.AreEqual(new DateTime(2025, 5, 6), earlyFinish);
         }
-        
+
         [TestMethod]
         public void CalculateLateStart_ShouldReturnCorrectDate_WhenThereArePreviousTasks()
         {
             var task1 = new Task(
-                "Task 1",                             
-                "Description of Task 1",              
-                new DateTime(2025, 5, 1),             
-                5,                                     
-                new List<Task>(),             
-                new List<Task>()              
+                "Task 1",
+                "Description of Task 1",
+                new DateTime(2025, 5, 1),
+                5,
+                new List<Task>(),
+                new List<Task>()
             );
 
             var task2 = new Task(
-                "Task 2",                            
-                "Description of Task 2",              
-                new DateTime(2025, 5, 6),             
-                3,                                     
-                new List<Task> { task1 },     
-                new List<Task>()               
+                "Task 2",
+                "Description of Task 2",
+                new DateTime(2025, 5, 6),
+                3,
+                new List<Task> { task1 },
+                new List<Task>()
             );
 
             DateTime lateStart = _taskService.CalculateLateStart(task2);
 
-           
+
             Assert.AreEqual(new DateTime(2025, 5, 6), lateStart);
         }
-        
+
         [TestMethod]
         public void CalculateLateFinish_ShouldReturnCorrectDate_WhenThereArePreviousTasks()
         {
             var task1 = new Task(
-                "Task 1",                             
-                "Description of Task 1",              
-                new DateTime(2025, 5, 1),            
-                5,                                     
-                new List<Task>(),              
-                new List<Task>()               
+                "Task 1",
+                "Description of Task 1",
+                new DateTime(2025, 5, 1),
+                5,
+                new List<Task>(),
+                new List<Task>()
             );
 
             var task2 = new Task(
-                "Task 2",                             
-                "Description of Task 2",              
-                new DateTime(2025, 5, 6),            
-                3,                                     
-                new List<Task> { task1 },      
-                new List<Task>()               
+                "Task 2",
+                "Description of Task 2",
+                new DateTime(2025, 5, 6),
+                3,
+                new List<Task> { task1 },
+                new List<Task>()
             );
 
             DateTime lateFinish = _taskService.CalculateLateFinish(task2);
 
-            
-            Assert.AreEqual(new DateTime(2025, 5, 9), lateFinish); 
+
+            Assert.AreEqual(new DateTime(2025, 5, 9), lateFinish);
         }
 
 
@@ -115,31 +116,31 @@ namespace Service.Test
         public void IsCritical_ShouldReturnTrue_WhenTaskHasNoFloat()
         {
             var task1 = new Task(
-                "Task 1",                             
-                "Description of Task 1",             
-                new DateTime(2025, 5, 1),             
-                5,                                    
-                new List<Task>(),              
-                new List<Task>()              
+                "Task 1",
+                "Description of Task 1",
+                new DateTime(2025, 5, 1),
+                5,
+                new List<Task>(),
+                new List<Task>()
             );
 
             var task2 = new Task(
-                "Task 2",                           
-                "Description of Task 2",             
-                new DateTime(2025, 5, 6),             
-                3,                                     
-                new List<Task> { task1 },     
-                new List<Task>()               
+                "Task 2",
+                "Description of Task 2",
+                new DateTime(2025, 5, 6),
+                3,
+                new List<Task> { task1 },
+                new List<Task>()
             );
 
-          
+
             bool isCritical = _taskService.IsCritical(task2);
 
-            
+
             Assert.IsTrue(isCritical);
         }
-   
-        
+
+
         [TestMethod]
         public void FromEntity_ShouldConvertTaskToTaskDTO()
         {
@@ -154,7 +155,7 @@ namespace Service.Test
 
             TaskDTO taskDTO = _taskService.FromEntity(task);
 
-            
+
             Assert.AreEqual(task.Title, taskDTO.Title);
             Assert.AreEqual(task.Description, taskDTO.Description);
             Assert.AreEqual(task.ExpectedStartDate, taskDTO.ExpectedStartDate);
@@ -178,15 +179,15 @@ namespace Service.Test
 
             Task task = _taskService.ToEntity(taskDTO);
 
-          
+
             Assert.AreEqual(taskDTO.Title, task.Title);
             Assert.AreEqual(taskDTO.Description, task.Description);
             Assert.AreEqual(taskDTO.ExpectedStartDate, task.ExpectedStartDate);
             Assert.AreEqual(taskDTO.Duration, task.Duration);
             Assert.AreEqual(taskDTO.State, task.State);
         }
-        
-       
+
+
         [TestMethod]
         public void FromEntity_ShouldMapStateCorrectly()
         {
@@ -198,14 +199,14 @@ namespace Service.Test
                 new List<Task>(),
                 new List<Task>()
             );
-    
-        
+
+
             task.State = State.DOING;
 
-           
+
             TaskDTO taskDTO = _taskService.FromEntity(task);
 
-         
+
             Assert.AreEqual(task.State, taskDTO.State);
         }
 
@@ -213,24 +214,24 @@ namespace Service.Test
         [ExpectedException(typeof(TaskServiceException))]
         public void CalculateEarlyFinish_ShouldThrowException_WhenTaskIsNull()
         {
-            _taskService.CalculateEarlyFinish(null);  
+            _taskService.CalculateEarlyFinish(null);
         }
-        
+
         [TestMethod]
-        [ExpectedException(typeof(TaskDurationException))]  
+        [ExpectedException(typeof(TaskDurationException))]
         public void CalculateEarlyFinish_ShouldThrowException_WhenDurationIsZeroOrNegative()
         {
             var task = new Task(
                 "Task 1",
                 "Description of Task 1",
                 new DateTime(2025, 5, 1),
-                0,  
+                0,
                 new List<Task>(),
                 new List<Task>()
             );
 
-            _taskService.CalculateEarlyFinish(task); 
-            
+            _taskService.CalculateEarlyFinish(task);
+
         }
 
         [TestMethod]
@@ -241,13 +242,37 @@ namespace Service.Test
                 "Task 1",
                 "Description of Task 1",
                 new DateTime(2025, 5, 1),
-                -1, 
+                -1,
                 new List<Task>(),
                 new List<Task>()
             );
 
             _taskService.CalculateEarlyFinish(task);
 
+        }
+        
+        [TestMethod]
+        public void ToEntity_ShouldConvertTaskDTOToTaskCorrectly()
+        {
+            var taskDTO = new TaskDTO
+            {
+                Title = "Task 1",
+                Description = "Description of Task 1",
+                ExpectedStartDate = new DateTime(2025, 5, 1),
+                Duration = 5,
+                PreviousTasks = new List<Task>(),
+                SameTimeTasks = new List<Task>(),
+                State = State.TODO
+            };
+
+            Task task = _taskService.ToEntity();
+
+            Assert.AreEqual(taskDTO.Title, task.Title);
+            Assert.AreEqual(taskDTO.Description, task.Description);
+            Assert.AreEqual(taskDTO.ExpectedStartDate, task.ExpectedStartDate);
+            Assert.AreEqual(taskDTO.Duration, task.Duration);
+            Assert.AreEqual(taskDTO.State, task.State);
+        }
 
 
 
