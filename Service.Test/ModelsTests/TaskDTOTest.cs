241db8b24e703c9e54c3fe2edcd0392278e5df5e
Santi <santicanadell0@gmail.com>
2025-05-05 22:13:19 -0300

    red: id not implemented in taskDTO

--- Service.Test/ModelsTests/TaskDTOTest.cs
+++ Service.Test/ModelsTests/TaskDTOTest.cs
@@ -9,56 +9,42 @@ public class TaskDTOTest
     [TestMethod]
     public void NewTask_WhenTitleIsNull_ThenTaskIsNotCreated()
     {
-       
-        TaskDTO user = new TaskDTO { Title = null };
-
-       
-        Assert.IsNull(user.Title);
+        TaskDTO taskDTO = new TaskDTO { Title = null };
+        Assert.IsNull(taskDTO.Title);
     }
     
     [TestMethod]
     public void NewTask_WhenDescriptionIsNull_ThenTaskIsNotCreated()
     {
-       
-        TaskDTO user = new TaskDTO { Description = null };
-
-       
-        Assert.IsNull(user.Description);
+        TaskDTO taskDTO = new TaskDTO { Description = null };
+        Assert.IsNull(taskDTO.Description);
     }
     
     [TestMethod]
     public void NewTask_WhenExpectedStartDateIsValid_ThenTaskIsCreated()
     {
-       
         DateTime validDate = new DateTime(2025, 5, 3); 
-        TaskDTO task = new TaskDTO { ExpectedStartDate = validDate };
-
-       
-        Assert.AreEqual(validDate, task.ExpectedStartDate);
+        TaskDTO taskDTO = new TaskDTO { ExpectedStartDate = validDate };
+        Assert.AreEqual(validDate, taskDTO.ExpectedStartDate);
     }
     
     [TestMethod]
     public void NewTask_WhenDurationIsValid_ThenTaskIsCreated()
     {
-      
         int validDuration = 5;
-        TaskDTO task = new TaskDTO { Duration = validDuration };
-
-   
-        Assert.AreEqual(validDuration, task.Duration);
+        TaskDTO taskDTO = new TaskDTO { Duration = validDuration };
+        Assert.AreEqual(validDuration, taskDTO.Duration);
     }
+
     [TestMethod]
     public void NewTask_WhenPreviousTasksAreAssigned_ThenPreviousTasksAreSet()
     {
-      
         var previousTasks = new List<Domain.Task>
         {
-            new Domain.Task("Task 1", "Description 1", DateTime.Now, 3, new List<Domain.Task>(), new List<Domain.Task>()),
-            new Domain.Task("Task 2", "Description 2", DateTime.Now, 5, new List<Domain.Task>(), new List<Domain.Task>())
+            new Domain.Task("Task 1", "Description 1", DateTime.Now, 3, new List<Domain.Task>(), new List<Domain.Task>(), new List<Resource>()),
+            new Domain.Task("Task 2", "Description 2", DateTime.Now, 5, new List<Domain.Task>(), new List<Domain.Task>(), new List<Resource>())
         };
         var taskDTO = new TaskDTO { PreviousTasks = previousTasks };
-
-   
         Assert.IsNotNull(taskDTO.PreviousTasks);
         Assert.AreEqual(2, taskDTO.PreviousTasks.Count);
         Assert.AreEqual("Task 1", taskDTO.PreviousTasks[0].Title);
@@ -68,25 +54,21 @@ public class TaskDTOTest
     [TestMethod]
     public void NewTask_WhenSameTimeTasksAreAssigned_ThenSameTimeTasksAreSet()
     {
-        // arrange
         var sameTimeTasks = new List<Domain.Task>
         {
-            new Domain.Task("Task A", "Description A", DateTime.Now, 3, new List<Domain.Task>(), new List<Domain.Task>()),
-            new Domain.Task("Task B", "Description B", DateTime.Now, 2, new List<Domain.Task>(), new List<Domain.Task>())
+            new Domain.Task("Task A", "Description A", DateTime.Now, 3, new List<Domain.Task>(), new List<Domain.Task>(), new List<Resource>()),
+            new Domain.Task("Task B", "Description B", DateTime.Now, 2, new List<Domain.Task>(), new List<Domain.Task>(), new List<Resource>())
         };
-        var taskDto = new TaskDTO { SameTimeTasks = sameTimeTasks };
-
-
-        Assert.IsNotNull(taskDto.SameTimeTasks);
-        Assert.AreEqual(2, taskDto.SameTimeTasks.Count);
-        Assert.AreEqual("Task A", taskDto.SameTimeTasks[0].Title);
-        Assert.AreEqual("Task B", taskDto.SameTimeTasks[1].Title);
+        var taskDTO = new TaskDTO { SameTimeTasks = sameTimeTasks };
+        Assert.IsNotNull(taskDTO.SameTimeTasks);
+        Assert.AreEqual(2, taskDTO.SameTimeTasks.Count);
+        Assert.AreEqual("Task A", taskDTO.SameTimeTasks[0].Title);
+        Assert.AreEqual("Task B", taskDTO.SameTimeTasks[1].Title);
     }
     
     [TestMethod]
     public void NewTask_WhenStateIsSet_ThenStateIsAssignedCorrectly()
     {
-     
         var stateTodo = State.TODO;
         var stateDoing = State.DOING;
         var stateDone = State.DONE;
@@ -95,14 +77,17 @@ public class TaskDTOTest
         var taskDoing = new TaskDTO { State = stateDoing };
         var taskDone = new TaskDTO { State = stateDone };
 
-   
         Assert.AreEqual<State>(stateTodo, taskTodo.State); 
         Assert.AreEqual<State>(stateDoing, taskDoing.State);
         Assert.AreEqual<State>(stateDone, taskDone.State);
     }
 
-
-
+    [TestMethod]
+    public void NewTask_WhenIdIsSet_ThenIdIsAssigned()
+    {
+        TaskDTO taskDTO = new TaskDTO { Id = 123 };
+        Assert.AreEqual(123, taskDTO.Id);
+    }
 
 
 }
