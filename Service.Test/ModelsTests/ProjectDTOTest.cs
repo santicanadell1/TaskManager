51fb0f3be567496dd2278f30fabad3777f706630
nbs282 <Norapala462@gmail.com>
2025-05-07 11:07:31 -0300

    Red: project dto is not created

---
+++ Service.Test/ModelsTests/ProjectDTOTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Service.Test.ModelsTests;
+
+[TestClass]
+public class ProjectDTOTests
+{
+    [TestMethod]
+    public void Validate_ShouldThrowValidationException_WhenNameIsNullOrWhiteSpace()
+    {
+        var projectDTO = new ProjectDTO
+        {
+            Name = "",
+            Description = "Test Description",
+            StartDate = DateTime.Now
+        };
+
+        var validationResults = new System.Collections.Generic.List<ValidationResult>();
+        bool isValid =
+            Validator.TryValidateObject(projectDTO, new ValidationContext(projectDTO), validationResults, true);
+
+        Assert.IsFalse(isValid);
+        Assert.AreEqual("Name is required.", validationResults[0].ErrorMessage);
+    }
+}
