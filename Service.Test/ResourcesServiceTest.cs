10e0bf16565c866ccfb9b3f4df8d4ad5d8e4f223
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-07 16:57:57 -0300

    Green: When adminProject updates a no exclusive resource for him throws exception

--- Service.Test/ResourcesServiceTest.cs
+++ Service.Test/ResourcesServiceTest.cs
@@ -292,6 +292,7 @@ public class ResourcesServiceTest
         
     }
     [TestMethod]
+    [ExpectedException(typeof(UnauthorizedAdminAccessException))]
     public void UpdateResource_ShouldThrowException_WhenResourceIsNotExclusiveForTheAdmin()
     {
         _loginService.LoginUser("adminSystem.user@example.com", "AdminPassword123@");
