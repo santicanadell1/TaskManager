0db1238f4f6b80e29bfe5372fb82dc5d461bbf42
nbs282 <Norapala462@gmail.com>
2025-05-07 11:41:45 -0300

    Red: admin proyect service is not created

---
+++ Service.Test/AdminPServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Models;
+using DataAccess;
+using Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Service.Test;
+
+[TestClass]
+public class AdminPServiceTests
+{
+    private InMemoryDatabase _database;
+    private AdminPService _service;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _database = new InMemoryDatabase();
+        _service = new AdminPService(_database);
+    }
+
+    [TestMethod]
+    public void CreateProject_ShouldAddProjectToDatabase_WhenValid()
+    {
+        var projectDTO = new ProjectDTO
+        {
+            Name = "New Project",
+            Description = "Project Description",
+        };
+
+        _service.CreateProject(projectDTO);
+
+        var project = _database.projects.GetProject(p => p.Name == projectDTO.Name);
+        Assert.IsNotNull(project);
+        Assert.AreEqual("New Project", project.Name);
+    }
+}
