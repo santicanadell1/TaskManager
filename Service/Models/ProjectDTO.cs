95d81ad2cd572b8f7f09102cf4eed076daf49b36
nbs282 <Norapala462@gmail.com>
2025-05-07 11:25:14 -0300

    Green: required description is implemented

--- Service/Models/ProjectDTO.cs
+++ Service/Models/ProjectDTO.cs
@@ -5,9 +5,10 @@ namespace Service.Models
 {
     public class ProjectDTO
     {
-        [Required(ErrorMessage = "Name is required.")]
+        [Required(ErrorMessage = "Project name is required.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Project description is required.")]
         public string Description { get; set; }
 
         public DateTime StartDate { get; set; }
