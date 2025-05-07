c89570798099f5992b26b565459f56c7ba2a464d
nbs282 <Norapala462@gmail.com>
2025-05-07 12:03:44 -0300

    Green: assign memebers to proyect is created

--- Service/AdminPService.cs
+++ Service/AdminPService.cs
@@ -25,4 +25,38 @@ public class AdminPService
         var newProject = new Project(projectDTO.Name, projectDTO.Description, projectDTO.StartDate);
         _database.projects.AddProject(newProject);
     }
+
+    public void AssignMembersToProject(string projectName, List<UserDTO> membersDTO)
+    {
+        var project = _database.projects.GetProject(p => p.Name == projectName);
+        if (project == null)
+        {
+            throw new ProjectNotFoundException();
+        }
+
+        foreach (var memberDTO in membersDTO)
+        {
+            var user = ToEntity(memberDTO);
+
+            if (!project.Members.Contains(user))
+            {
+                project.AddMember(user);
+            }
+        }
+
+        _database.projects.UpdateProject(projectName, project);
+    }
+
+    private User ToEntity(UserDTO userDTO)
+    {
+        return new User
+        {
+            FirstName = userDTO.FirstName,
+            LastName = userDTO.LastName,
+            Email = userDTO.Email,
+            Birthday = userDTO.Birthday,
+            Password = userDTO.Password,
+            Roles = userDTO.Roles
+        };
+    }
 }
