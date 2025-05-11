997242ca3f6aa7d4289e141951a705af5efa5ff7
SantiCanadell <santicanadell0@gmail.com>
2025-05-10 23:35:29 -0300

    Red: error in CalculateCriticalPath_ShouldIdentifyCriticalPath_WithComplexDependencies

--- Service.Test/CPMServiceTest.cs
+++ Service.Test/CPMServiceTest.cs
@@ -15,6 +15,8 @@ namespace Service.Test
         private CpmService _cpmService;
         private Task _taskA;
         private Task _taskB;
+        private Task _taskC;
+        private Task _taskD;
 
         [TestInitialize]
         public void Setup()
@@ -37,11 +39,33 @@ namespace Service.Test
                 "Descripción de Tarea B",
                 new DateTime(2025, 1, 1),
                 4,
-                new List<Task> { _taskA },
+                new List<Task>(),
                 new List<Task>(),
                 new List<Resource>()
             );
             _taskB.Id = 2;
+
+            _taskC = new Task(
+                "Tarea C",
+                "Descripción de Tarea C",
+                new DateTime(2025, 1, 1),
+                2,
+                new List<Task> { _taskA },
+                new List<Task>(),
+                new List<Resource>()
+            );
+            _taskC.Id = 3;
+
+            _taskD = new Task(
+                "Tarea D",
+                "Descripción de Tarea D",
+                new DateTime(2025, 1, 1),
+                3,
+                new List<Task> { _taskB, _taskC },
+                new List<Task>(),
+                new List<Resource>()
+            );
+            _taskD.Id = 4;
         }
 
         [TestMethod]
@@ -90,21 +114,33 @@ namespace Service.Test
         [TestMethod]
         public void CalculateCriticalPath_ShouldCalculateEarlyDates_ForTaskWithPredecessors()
         {
-            var tasks = new List<Task> { _taskA, _taskB };
+            var tasks = new List<Task> { _taskA, _taskB, _taskC, _taskD };
             var result = _cpmService.CalculateCriticalPath(tasks);
 
             var taskA = result.AllTasks.First(t => t.Id == 1);
-            var taskB = result.AllTasks.First(t => t.Id == 2);
+            var taskC = result.AllTasks.First(t => t.Id == 3);
             
             Assert.AreEqual(_taskA.ExpectedStartDate, taskA.StartDate);
-            Assert.AreEqual(taskA.EndDate, taskB.StartDate);
-            Assert.AreEqual(taskB.StartDate.AddDays(taskB.Duration), taskB.EndDate);
+            Assert.AreEqual(taskA.EndDate, taskC.StartDate);
+            Assert.AreEqual(taskC.StartDate.AddDays(taskC.Duration), taskC.EndDate);
         }
 
         [TestMethod]
         public void CalculateCriticalPath_ShouldCalculateLateDates_AndSlack()
         {
             var tasks = new List<Task> { _taskA, _taskB };
+            var taskBWithDependency = new Task(
+                "Tarea B",
+                "Descripción de Tarea B",
+                new DateTime(2025, 1, 1),
+                4,
+                new List<Task> { _taskA },
+                new List<Task>(),
+                new List<Resource>()
+            );
+            taskBWithDependency.Id = 2;
+            
+            tasks = new List<Task> { _taskA, taskBWithDependency };
             var result = _cpmService.CalculateCriticalPath(tasks);
 
             var taskA = result.AllTasks.First(t => t.Id == 1);
@@ -120,6 +156,33 @@ namespace Service.Test
             
             Assert.IsTrue(taskA.IsCritical);
             Assert.IsTrue(taskB.IsCritical);
+        }
+
+        [TestMethod]
+        public void CalculateCriticalPath_ShouldIdentifyCriticalPath_WithComplexDependencies()
+        {
+            var tasks = new List<Task> { _taskA, _taskB, _taskC, _taskD };
+            var result = _cpmService.CalculateCriticalPath(tasks);
+
+            Assert.IsTrue(result.CriticalPath.Count > 0);
+            Assert.IsTrue(result.CriticalTasks.Count > 0);
+            
+            Assert.IsTrue(result.CriticalTasks.Any(t => t.Id == 4));
+            
+            var nonCriticalTasks = result.AllTasks.Where(t => !t.IsCritical).ToList();
+            foreach (var task in nonCriticalTasks)
+            {
+                Assert.IsTrue(task.Slack.TotalDays > 0);
+            }
+            
+            var criticalTasks = result.AllTasks.Where(t => t.IsCritical).ToList();
+            foreach (var task in criticalTasks)
+            {
+                Assert.AreEqual(0, task.Slack.TotalDays, 0.0001);
+            }
+            
+            Assert.IsTrue(result.CriticalPath.First().Id == 2);
+            Assert.IsTrue(result.CriticalPath.Last().Id == 4);
         }
     }
 }
