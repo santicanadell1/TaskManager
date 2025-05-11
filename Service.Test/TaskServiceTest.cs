4ede0ae5d5f2b345f80c9778e3aad30a22689e45
SantiCanadell <santicanadell0@gmail.com>
2025-05-11 19:18:38 -0300

    Red: AddTask_ShouldThrowException_WhenProjectDoesNotExist not working

--- Service.Test/TaskServiceTest.cs
+++ Service.Test/TaskServiceTest.cs
@@ -1,150 +1,201 @@
 ﻿using DataAccess;
+using DataAccess.ProjectRepositoryExceptions;
 using Domain;
-using Domain.Exceptions;
+using Domain.Exceptions.TaskRepositoryExceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Task = Domain.Task;
 
-namespace Service.Test;
-
-[TestClass]
-public class TaskServiceTest
+namespace Service.Test
 {
-    private InMemoryDatabase _database;
-    private TaskService _taskService;
-    private Project _genericProject;
-    private Task _task1;
-    private Task _task2;
-    private TaskDTO _taskDTO1;
-    private TaskDTO _taskDTO2;
-
-    [TestInitialize]
-    public void Setup()
+    [TestClass]
+    public class TaskServiceTest
     {
-        _database = new InMemoryDatabase();
-        _taskService = new TaskService(_database);
+        private InMemoryDatabase _database;
+        private TaskService _taskService;
+        private Project _genericProject;
+        private Task _task1;
+        private Task _task2;
+        private TaskDTO _taskDTO1;
+        private TaskDTO _taskDTO2;
+        private Resource _resource1;
+        private Resource _resource2;
+        private ResourceDTO _resourceDTO1;
+        private ResourceDTO _resourceDTO2;
 
-        _genericProject = new Project("Generic Project", "Description", DateTime.Now);
-        _database.projects.AddProject(_genericProject);
-
-        _taskDTO1 = new TaskDTO
+        [TestInitialize]
+        public void Setup()
         {
-            Title = "Task 1",
-            Description = "Description of Task 1",
-            ExpectedStartDate = DateTime.Now,
-            Duration = 5,
-            PreviousTasks = new List<TaskDTO>(),
-            SameTimeTasks = new List<TaskDTO>(),
-            Resources = new List<ResourceDTO>()
-        };
+            _database = new InMemoryDatabase();
+            _taskService = new TaskService(_database);
 
-        _taskDTO2 = new TaskDTO
+            _genericProject = new Project("Generic Project", "Description", DateTime.Now);
+            _database.projects.AddProject(_genericProject);
+
+            _resource1 = new Resource("Resource 1", "Type 1", "Description 1") { Id = 1 };
+            _resource2 = new Resource("Resource 2", "Type 2", "Description 2") { Id = 2 };
+
+            _resourceDTO1 = new ResourceDTO
+            {
+                Id = 1,
+                Name = "Resource 1",
+                Type = "Type 1",
+                Description = "Description 1"
+            };
+
+            _resourceDTO2 = new ResourceDTO
+            {
+                Id = 2,
+                Name = "Resource 2",
+                Type = "Type 2",
+                Description = "Description 2"
+            };
+
+            _taskDTO1 = new TaskDTO
+            {
+                Title = "Task 1",
+                Description = "Description of Task 1",
+                ExpectedStartDate = DateTime.Now,
+                Duration = 5,
+                PreviousTasks = new List<TaskDTO>(),
+                SameTimeTasks = new List<TaskDTO>(),
+                Resources = new List<ResourceDTO> { _resourceDTO1 },
+                State = StateDTO.TODO
+            };
+
+            _taskDTO2 = new TaskDTO
+            {
+                Title = "Task 2",
+                Description = "Description of Task 2",
+                ExpectedStartDate = DateTime.Now.AddDays(2),
+                Duration = 3,
+                PreviousTasks = new List<TaskDTO>(),
+                SameTimeTasks = new List<TaskDTO>(),
+                Resources = new List<ResourceDTO> { _resourceDTO2 },
+                State = StateDTO.DOING
+            };
+
+            _task1 = new Task(
+                _taskDTO1.Title,
+                _taskDTO1.Description,
+                _taskDTO1.ExpectedStartDate,
+                _taskDTO1.Duration,
+                new List<Task>(),
+                new List<Task>(),
+                new List<Resource> { _resource1 }
+            );
+            _task1.Id = 1;
+
+            _task2 = new Task(
+                _taskDTO2.Title,
+                _taskDTO2.Description,
+                _taskDTO2.ExpectedStartDate,
+                _taskDTO2.Duration,
+                new List<Task>(),
+                new List<Task>(),
+                new List<Resource> { _resource2 }
+            );
+            _task2.Id = 2;
+
+            _database.projects.AddTask("Generic Project", _task1);
+            _database.projects.AddTask("Generic Project", _task2);
+        }
+
+        [TestMethod]
+        public void AddTask_ShouldAddTask_WhenTaskIsValid()
         {
-            Title = "Task 2",
-            Description = "Description of Task 2",
-            ExpectedStartDate = DateTime.Now.AddDays(2),
-            Duration = 3,
-            PreviousTasks = new List<TaskDTO>(),
-            SameTimeTasks = new List<TaskDTO>(),
-            Resources = new List<ResourceDTO>()
-        };
+            var taskDTO = new TaskDTO
+            {
+                Title = "Test Task",
+                Description = "Test Description",
+                ExpectedStartDate = DateTime.Now.AddDays(1),
+                Duration = 5,
+                PreviousTasks = new List<TaskDTO>(),
+                SameTimeTasks = new List<TaskDTO>(),
+                Resources = new List<ResourceDTO>()
+            };
 
-        _task1 = new Task(
-            _taskDTO1.Title,
-            _taskDTO1.Description,
-            _taskDTO1.ExpectedStartDate,
-            _taskDTO1.Duration,
-            new List<Task>(),
-            new List<Task>(),
-            new List<Resource>()
-        );
-        _task2 = new Task(
-            _taskDTO2.Title,
-            _taskDTO2.Description,
-            _taskDTO2.ExpectedStartDate,
-            _taskDTO2.Duration,
-            new List<Task>(),
-            new List<Task>(),
-            new List<Resource>()
-        );
+            _taskService.AddTask("Generic Project", taskDTO);
 
-        _database.projects.AddTask("Generic Project", _task1);
-        _database.projects.AddTask("Generic Project", _task2);
-    }
+            var project = _database.projects.GetProject(p => p.Name == "Generic Project");
+            var tasks = project.Tasks;
+            Assert.AreEqual(3, tasks.Count);
+            Assert.AreEqual("Test Task", tasks[2].Title);
+        }
 
-    [TestMethod]
-    public void AddTask_ShouldAddTask_WhenTaskIsValid()
-    {
-        var taskDTO = new TaskDTO
+        [TestMethod]
+        [ExpectedException(typeof(ProjectNotFoundException))]
+        public void AddTask_ShouldThrowException_WhenProjectDoesNotExist()
         {
-            Title = "Test Task",
-            Description = "Test Description",
-            ExpectedStartDate = DateTime.Now.AddDays(1),
-            Duration = 5,
-            PreviousTasks = new List<TaskDTO>(),
-            SameTimeTasks = new List<TaskDTO>(),
-            Resources = new List<ResourceDTO>()
-        };
+            var taskDTO = new TaskDTO
+            {
+                Title = "Test Task",
+                Description = "Test Description",
+                ExpectedStartDate = DateTime.Now.AddDays(1),
+                Duration = 5
+            };
 
-        _taskService.AddTask("Generic Project", taskDTO);
+            _taskService.AddTask("Non-Existent Project");
+        }
 
-        var project = _database.projects.GetProject(p => p.Name == "Generic Project");
-        var tasks = project.Tasks;
-        Assert.AreEqual(3, tasks.Count);
-        Assert.AreEqual("Test Task", tasks[2].Title);
-    }
-
-    [TestMethod]
-    public void DeleteTask_ShouldDeleteTask_WhenTaskExists()
-    {
-        _taskService.DeleteTask("Generic Project", _task1.Id);
-
-        var project = _database.projects.GetProject(p => p.Name == "Generic Project");
-        Assert.AreEqual(1, project.Tasks.Count);
-        Assert.AreEqual("Task 2", project.Tasks[0].Title);
-    }
-
-    [TestMethod]
-    public void UpdateTask_ShouldUpdateTask_WhenTaskExists()
-    {
-        var updateDTO = new TaskDTO
+        [TestMethod]
+        public void DeleteTask_ShouldDeleteTask_WhenTaskExists()
         {
-            Title = "Updated Task 1",
-            Description = "Updated Description",
-            ExpectedStartDate = DateTime.Now.AddDays(1),
-            Duration = 6,
-            PreviousTasks = new List<TaskDTO>(),
-            SameTimeTasks = new List<TaskDTO>(),
-            State = StateDTO.TODO,
-            Resources = new List<ResourceDTO>()
-        };
+            _taskService.DeleteTask("Generic Project", _task1.Id);
 
-        _taskService.UpdateTask("Generic Project", _task1.Id, updateDTO);
+            var project = _database.projects.GetProject(p => p.Name == "Generic Project");
+            Assert.AreEqual(1, project.Tasks.Count);
+            Assert.AreEqual("Task 2", project.Tasks[0].Title);
+        }
 
-        var project = _database.projects.GetProject(p => p.Name == "Generic Project");
-        var updatedTask = project.Tasks.FirstOrDefault(t => t.Id == _task1.Id);
+        [TestMethod]
+        public void UpdateTask_ShouldUpdateTask_WhenTaskExists()
+        {
+            var updateDTO = new TaskDTO
+            {
+                Title = "Updated Task 1",
+                Description = "Updated Description",
+                ExpectedStartDate = DateTime.Now.AddDays(1),
+                Duration = 6,
+                PreviousTasks = new List<TaskDTO>(),
+                SameTimeTasks = new List<TaskDTO>(),
+                State = StateDTO.TODO,
+                Resources = new List<ResourceDTO>()
+            };
 
-        Assert.AreEqual("Updated Task 1", updatedTask.Title);
-        Assert.AreEqual("Updated Description", updatedTask.Description);
-        Assert.AreEqual(6, updatedTask.Duration);
-    }
+            _taskService.UpdateTask("Generic Project", _task1.Id, updateDTO);
 
-    [TestMethod]
-    public void GetTasks_ShouldReturnAllTasks_WhenProjectExists()
-    {
-        var tasks = _taskService.GetTasks("Generic Project");
+            var project = _database.projects.GetProject(p => p.Name == "Generic Project");
+            var updatedTask = project.Tasks.FirstOrDefault(t => t.Id == _task1.Id);
 
-        Assert.AreEqual(2, tasks.Count);
-        Assert.AreEqual("Task 1", tasks[0].Title);
-        Assert.AreEqual("Task 2", tasks[1].Title);
-    }
+            Assert.AreEqual("Updated Task 1", updatedTask.Title);
+            Assert.AreEqual("Updated Description", updatedTask.Description);
+            Assert.AreEqual(6, updatedTask.Duration);
+        }
 
-    [TestMethod]
-    public void GetTask_ShouldReturnTask_WhenTaskExists()
-    {
-        var taskDTO = _taskService.GetTask("Generic Project", _task1.Id);
+        [TestMethod]
+        public void GetTasks_ShouldReturnAllTasks_WhenProjectExists()
+        {
+            var tasks = _taskService.GetTasks("Generic Project");
 
-        Assert.AreEqual("Task 1", taskDTO.Title);
-        Assert.AreEqual("Description of Task 1", taskDTO.Description);
-        Assert.AreEqual(5, taskDTO.Duration);
+            Assert.AreEqual(2, tasks.Count);
+            Assert.AreEqual("Task 1", tasks[0].Title);
+            Assert.AreEqual("Task 2", tasks[1].Title);
+        }
+
+        [TestMethod]
+        public void GetTask_ShouldReturnTask_WhenTaskExists()
+        {
+            var taskDTO = _taskService.GetTask("Generic Project", _task1.Id);
+
+            Assert.AreEqual("Task 1", taskDTO.Title);
+            Assert.AreEqual("Description of Task 1", taskDTO.Description);
+            Assert.AreEqual(5, taskDTO.Duration);
+        }
+
+
     }
 }
