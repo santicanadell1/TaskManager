1f424f7c667904852313b2eef422f0f0bcbfa219
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-11 20:28:18 -0300

    Green: When getting all project for a user that is not a member, it should throw an exception

--- Service/MemberPService.cs
+++ Service/MemberPService.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess;
 using Domain;
+using Domain.Exceptions;
 using Service.Models;
 
 namespace Service;
@@ -19,6 +20,10 @@ public class MemberPService
         List<ProjectDTO> projectsFromMember = new List<ProjectDTO>();
         List<ProjectDTO> projects = adminPService.GetAllProjects();
         UserDTO user = userService.GetUser(email);
+        if (!user.Roles.Contains(Rol.ProjectMember))
+        {
+            throw new UserIsNotAMemberException();
+        }
         foreach (var project in projects)
         {
             if (project.Members.Any(m => m.Email == user.Email))
