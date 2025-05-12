fee931683a3ff4d5abb61252e1ab4a1b699470da
SantiCanadell <santicanadell0@gmail.com>
2025-05-12 20:05:19 -0300

    Refactor: unnesessary comments on NotificationRepository

--- DataAccess.Tests/NotificationRepositoryTest.cs
+++ DataAccess.Tests/NotificationRepositoryTest.cs
@@ -10,27 +10,21 @@ public class NotificationRepositoryTest
     [TestMethod]
     public void NewNotificationRepository_WhenRepositoryIsCreated_ShouldNotBeNull()
     {
-        // Arrange
         NotificationRepository notificationRepository;
 
-        // Act
         notificationRepository = new NotificationRepository();
 
-        // Assert
         Assert.IsNotNull(notificationRepository);
     }
     
     [TestMethod]
     public void AddNewNotification_WhenAddNewNotification_ListShouldContainNotification()
     {
-        // Arrange
         NotificationRepository notificationRepository = new NotificationRepository();
         Notification notification = new Notification(false, "New notification");
 
-        // Act
         notificationRepository.AddNotification(notification);
 
-        // Assert
         Assert.IsTrue(notificationRepository.GetAll().Contains(notification));
     }
     [TestMethod]
