2d04d6b5f4e640398707a4c9cc579aa15e5f9f37
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-12 12:14:04 -0300

    Green: When initializing with correct data it should initialize correectly

--- Service.Test/ModelsTests/GanttDataTest.cs
+++ Service.Test/ModelsTests/GanttDataTest.cs
@@ -28,7 +28,25 @@ namespace Service.Test.ModelsTests;
         [TestMethod]
         public void GanttData_ShouldHoldProvidedData()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var task = new GanttTask { id = 1, text = "Tarea 1", start_date = "2025-05-10", duration = 5, progress = 0.5 };
+            var link = new GanttLink { id = 1, source = 1, target = 2, type = "0", critical = true };
+            var criticalIds = new List<int> { 1, 2, 3 };
+
+            var ganttData = new GanttData
+            {
+                data = new List<GanttTask> { task },
+                links = new List<GanttLink> { link },
+                criticalPathIds = criticalIds
+            };
+
+            // Assert
+            Assert.AreEqual(1, ganttData.data.Count);
+            Assert.AreEqual("Tarea 1", ganttData.data[0].text);
+            Assert.AreEqual(1, ganttData.links.Count);
+            Assert.IsTrue(ganttData.links[0].critical);
+            Assert.AreEqual(3, ganttData.criticalPathIds.Count);
+            CollectionAssert.AreEqual(criticalIds, ganttData.criticalPathIds);
         }
         
     }
