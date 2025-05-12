8bcdba9db6a4d7534a9b105885dd8bda2fc08935
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-12 11:26:33 -0300

    Refactor: Removing comments and empty lines

--- Service.Test/ModelsTests/GanttLinkTest.cs
+++ Service.Test/ModelsTests/GanttLinkTest.cs
@@ -8,38 +8,25 @@ public class GanttLinkTest
     [TestMethod]
     public void GanttLink_WhenInitializing_ShouldInitializeWithDefaultType()
     {
-        // Arrange
         var link = new GanttLink();
-
-        // Act
         var type = link.type;
-
-        // Assert
         Assert.AreEqual("0", type);
     }
     [TestMethod]
     public void GanttLink_WhenInitializingCritical_ShouldBeFalseByDefault()
     {
-        // Arrange
         var link = new GanttLink();
-
-        // Assert
         Assert.IsFalse(link.critical);
     }
     [TestMethod]
     public void GanttLink_WhenInitializingAType_ShouldAllowCustomValue()
     {
-        // Arrange
         var link = new GanttLink { type = "2" };
-
-        // Assert
         Assert.AreEqual("2", link.type);
     }
-    
     [TestMethod]
     public void GanttLink_ShouldSetAllPropertiesCorrectly()
     {
-        // Arrange
         var link = new GanttLink
         {
             id = 1,
@@ -48,8 +35,6 @@ public class GanttLinkTest
             type = "1",
             critical = true
         };
-
-        // Assert
         Assert.AreEqual(1, link.id);
         Assert.AreEqual(101, link.source);
         Assert.AreEqual(202, link.target);
