7e45a61eaad69678aed605b68a9265b1d08b0b72
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-12 12:12:56 -0300

    Green: When initializing with empty lists it should initialize correectly

--- Service.Test/ModelsTests/GanttDataTest.cs
+++ Service.Test/ModelsTests/GanttDataTest.cs
@@ -1,6 +1,30 @@
-﻿namespace Service.Test.ModelsTests;
+﻿using Service.Models;
 
-public class GanttDataTest
-{
-    
-}
+namespace Service.Test.ModelsTests;
+
+[TestClass]
+    public class GanttDataTests
+    {
+        [TestMethod]
+        public void GanttData_ShouldInitializeWithEmptyLists()
+        {
+            // Arrange
+            var ganttData = new GanttData
+            {
+                data = new List<GanttTask>(),
+                links = new List<GanttLink>(),
+                criticalPathIds = new List<int>()
+            };
+
+            // Assert
+            Assert.IsNotNull(ganttData.data);
+            Assert.IsNotNull(ganttData.links);
+            Assert.IsNotNull(ganttData.criticalPathIds);
+            Assert.AreEqual(0, ganttData.data.Count);
+            Assert.AreEqual(0, ganttData.links.Count);
+            Assert.AreEqual(0, ganttData.criticalPathIds.Count);
+        }
+
+
+        
+    }
--- Service/Models/GanttData.cs
+++ Service/Models/GanttData.cs
@@ -2,5 +2,7 @@
 
 public class GanttData
 {
-    
+    public List<GanttTask> data { get; set; }
+    public List<GanttLink> links { get; set; }
+    public List<int> criticalPathIds { get; set; } 
 }
