cc226f38ca5e52f89c9f709ed8649ea7e9436cbc
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-11 21:32:17 -0300

    Refactor: Creating interface for MemberPService

---
+++ Service/Interfaces/IMemberPService.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Service.Models;
+
+namespace Service.Interfaces;
+
+public interface IMemberPService
+{
+    List<ProjectDTO> GetAllProjectsFromAMember(string email);
+    void CheckUserRole(string email);
+    void CheckIsTaskOfTheUser(int taskId, string email);
+    void ChangeTaskStatus(string projectName, string email, TaskDTO task, State status);
+}
