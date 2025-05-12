ce1d0bdf283f88e02bb076dfbd58d1d1d6dd5e76
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-12 11:55:23 -0300

    Green: When initializing with correct values it should initialize correctly

--- Service/Models/GanttTask.cs
+++ Service/Models/GanttTask.cs
@@ -2,6 +2,11 @@
 
 public class GanttTask
 {
+    public int id { get; set; }
+    public string text { get; set; }
+    public string start_date { get; set; }
+    public string end_date { get; set; } 
+    public int duration { get; set; }
     public bool critical { get; set; } 
     public double slack { get; set; }
     public double progress { get; set; }
