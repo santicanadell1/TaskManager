3009017132d0b00ea8809112381b02eb2b69cdfa
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-11 21:33:01 -0300

    Refactor: Service Uses Interface

--- Service/Interfaces/IMemberPService.cs
+++ Service/Interfaces/IMemberPService.cs
@@ -6,7 +6,5 @@ namespace Service.Interfaces;
 public interface IMemberPService
 {
     List<ProjectDTO> GetAllProjectsFromAMember(string email);
-    void CheckUserRole(string email);
-    void CheckIsTaskOfTheUser(int taskId, string email);
     void ChangeTaskStatus(string projectName, string email, TaskDTO task, State status);
 }
