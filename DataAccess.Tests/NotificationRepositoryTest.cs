b313637787b729c03452340f4c26096165e5df20
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-13 17:55:15 -0300

    Fix: Notification repository tests

--- DataAccess.Tests/NotificationRepositoryTest.cs
+++ DataAccess.Tests/NotificationRepositoryTest.cs
@@ -21,7 +21,7 @@ public class NotificationRepositoryTest
     public void AddNewNotification_WhenAddNewNotification_ListShouldContainNotification()
     {
         NotificationRepository notificationRepository = new NotificationRepository();
-        Notification notification = new Notification(false, "New notification");
+        Notification notification = new Notification(false, "New notification", new Project());
 
         notificationRepository.AddNotification(notification);
 
@@ -31,8 +31,8 @@ public class NotificationRepositoryTest
     public void AddNewNotification_WhenGettingANotification_ShouldReturnNotification()
     {
         NotificationRepository notificationRepository = new NotificationRepository();
-        Notification notification1 = new Notification(false, "New notification");
-        Notification notification2 = new Notification(true, "Another notification");
+        Notification notification1 = new Notification(false, "New notification", new Project());
+        Notification notification2 = new Notification(true, "Another notification", new Project());
         notificationRepository.AddNotification(notification1);
         notificationRepository.AddNotification(notification2);
 
@@ -45,8 +45,8 @@ public class NotificationRepositoryTest
     public void UpdateNotification_WhenGettingTheNotification_ShouldBeDifferentFromOriginalNotification()
     {
         NotificationRepository notificationRepository = new NotificationRepository();
-        Notification notification1 = new Notification(false, "Old notification");
-        Notification notification2 = new Notification(true, "Updated notification");
+        Notification notification1 = new Notification(false, "Old notification" , new Project());
+        Notification notification2 = new Notification(true, "Updated notification" , new Project());
         notificationRepository.AddNotification(notification1);
 
         notificationRepository.Update(notification1, notification2);
@@ -58,11 +58,11 @@ public class NotificationRepositoryTest
     public void UpdateNotification_WhenNotificationIsNotFound_ShouldThrowNotificationNotFoundException()
     {
         NotificationRepository notificationRepository = new NotificationRepository();
-        Notification notification1 = new Notification(false, "Old notification");
-        Notification notification2 = new Notification(true, "Updated notification");
+        Notification notification1 = new Notification(false, "Old notification" , new Project());
+        Notification notification2 = new Notification(true, "Updated notification" , new Project());
         notificationRepository.AddNotification(notification1);
 
-        Notification notification3 = new Notification(false, "Nonexistent notification");
+        Notification notification3 = new Notification(false, "Nonexistent notification" , new Project());
         notificationRepository.Update(notification3, notification2);
     }
     
@@ -70,8 +70,8 @@ public class NotificationRepositoryTest
     public void DeleteNotification_WhenGettingTheNotification_ShouldBeNull()
     {
         NotificationRepository notificationRepository = new NotificationRepository();
-        Notification notification1 = new Notification(false, "Notification to be deleted");
-        Notification notification2 = new Notification(true, "Another notification");
+        Notification notification1 = new Notification(false, "Notification to be deleted", new Project());
+        Notification notification2 = new Notification(true, "Another notification" , new Project());
         notificationRepository.AddNotification(notification1);
         notificationRepository.AddNotification(notification2);
 
