ee0970e2c873a11f02896fa31e0a07d1482943a2
Santi <santicanadell0@gmail.com>
2025-05-14 14:27:52 -0300

    refactor: deleting unnecessary comments in Program

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -16,17 +16,14 @@ builder.Services.AddScoped<CpmService>();
 builder.Services.AddScoped<GanttService>();
 builder.Services.AddScoped<NotificationService>();
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
