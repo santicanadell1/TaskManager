796b78b7293beb703c9017af5bb32503c61e513a
FeliD <FS281987@fi365.ort.edu.uy>
2025-05-15 12:14:38 -0300

    Refactor: Deleting comments

--- Service/PasswordManager.cs
+++ Service/PasswordManager.cs
@@ -16,21 +16,14 @@ public class PasswordManager : IPasswordManager
         var passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$";
         return Regex.IsMatch(password, passwordPattern);
     }
-
-
-    // Hashing function using SHA-256 algorithm to hash the password
     public string HashPassword(string password)
     {
-        // Create an SHA256 instance
         using (var sha256Hash = SHA256.Create())
         {
-            // Convert the password string into a byte array
             var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-            // Convert the byte array to a hexadecimal string
             var builder = new StringBuilder();
             foreach (var b in
-                     bytes) builder.Append(b.ToString("x2")); // Convert each byte to a two-character hexadecimal string
+                     bytes) builder.Append(b.ToString("x2")); 
 
             return builder.ToString();
         }
