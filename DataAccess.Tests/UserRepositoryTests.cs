7a55d1439bd2697a0d27b6c00ae9e1251bea0b39
nbs282 <Norapala462@gmail.com>
2025-05-30 18:44:03 -0300

    Refactor tests in UserRepositoryTest

--- DataAccess.Tests/UserRepositoryTests.cs
+++ DataAccess.Tests/UserRepositoryTests.cs
@@ -6,106 +6,99 @@ namespace DataAccess.Test;
 [TestClass]
 public class UserRepositoryTests
 {
-    
-    protected readonly AppDbContext _db;
+    private UserRepository _userRepository;
+    private AppDbContext _context;
+    private InMemoryAppContextFactory _contextFactory;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _contextFactory = new InMemoryAppContextFactory();
+        _context = _contextFactory.CreateDbContext();
+        _userRepository = new UserRepository(_context);
+    }
+
+    [TestCleanup]
+    public void CleanUp()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
     [TestMethod]
     public void NewUserRepository_WhenRepositoryIsCreated_ShouldNotBeNull()
     {
-        UserRepository userRepository;
-        userRepository = new UserRepository(_db);
-        Assert.IsNotNull(userRepository);
+        Assert.IsNotNull(_userRepository);
     }
 
     [TestMethod]
     public void AddNewUser_WhenAddNewUser_ListShouldContainUser()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        Assert.IsTrue(userRepository.GetAll().Contains(user));
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        Assert.IsTrue(_userRepository.GetAll().Contains(user));
     }
 
     [TestMethod]
     [ExpectedException(typeof(UserEmailIsDuplicatedException))]
     public void AddNewUser_IfUserEmailAlreadyExists_ShouldThrowUseEmailIsDuplicatedException()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.AddUser(user2);
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.AddUser(user2);
     }
 
     [TestMethod]
     public void AddNewUser_WhenGettingAUser_ShouldReturnUser()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.AddUser(user2);
-        var user3 = userRepository.Get(u => u.Email == "Email1@email.com");
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.AddUser(user2);
+        var user3 = _userRepository.Get(u => u.Email == "Email1@email.com");
         Assert.AreEqual(user, user3);
     }
 
     [TestMethod]
-    public void updateAUser_WhenGettingTheUser_ShouldBeDifferentFromTheOriginalUser()
+    public void UpdateAUser_WhenGettingTheUser_ShouldBeDifferentFromTheOriginalUser()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.Update(user.Email, user2);
-        Assert.AreNotEqual(user, userRepository.Get(u => u.Email == "Email1@email.com"));
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.Update(user.Email, user2);
+        Assert.AreNotEqual(user, _userRepository.Get(u => u.Email == "Email1@email.com"));
     }
 
     [TestMethod]
     [ExpectedException(typeof(UserNotFoundException))]
-    public void updateAUser_WhenEmailIsNotFound_ThrowUserNotFoundException()
+    public void UpdateAUser_WhenEmailIsNotFound_ThrowUserNotFoundException()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.Update("EmailDiferente@email.com", user2);
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.Update("EmailDiferente@email.com", user2);
     }
 
     [TestMethod]
     [ExpectedException(typeof(UserEmailIsDuplicatedException))]
-    public void updateAUser_WhenEmailAlreadyExists_ThrowUserEmailIsDuplicatedException()
+    public void UpdateAUser_WhenEmailAlreadyExists_ThrowUserEmailIsDuplicatedException()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user3 = new User("First Name 3", "Last Name 3", "Email2@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.AddUser(user2);
-        userRepository.Update("Email1@email.com", user3);
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user3 = new User("First Name 3", "Last Name 3", "Email2@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.AddUser(user2);
+        _userRepository.Update("Email1@email.com", user3);
     }
 
     [TestMethod]
     public void DeleteAUser_WhenGettingTheUser_ShouldBeNull()
     {
-        var userRepository = new UserRepository(_db);
-        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18),
-            "Password");
-        userRepository.AddUser(user);
-        userRepository.AddUser(user2);
-        userRepository.Delete(user.Email);
-        Assert.IsNull(userRepository.Get(u => u.Email == "Email1@email.com"));
+        var user = new User("First Name 1", "Last Name 1", "Email1@email.com", DateTime.Today.AddYears(-18), "Password");
+        var user2 = new User("First Name 2", "Last Name 2", "Email2@email.com", DateTime.Today.AddYears(-18), "Password");
+        _userRepository.AddUser(user);
+        _userRepository.AddUser(user2);
+        _userRepository.Delete(user.Email);
+        Assert.IsNull(_userRepository.Get(u => u.Email == "Email1@email.com"));
     }
 }
