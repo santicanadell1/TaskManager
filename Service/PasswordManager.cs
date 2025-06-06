9253ca937f14d552470b2b4121d99eab62aa8d9a
nbs282 <Norapala462@gmail.com>
2025-06-06 18:56:35 -0300

    refactor password M service tests and passowrd M service

--- Service/PasswordManager.cs
+++ Service/PasswordManager.cs
@@ -7,23 +7,23 @@ public class PasswordManager : IPasswordManager
 {
     public string getDefaultPassword()
     {
-        var defaultPassword = "Password123?";
+        string defaultPassword = "Password123?";
         return defaultPassword;
     }
 
     public bool IsValidPassword(string password)
     {
-        var passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$";
+        string passwordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$";
         return Regex.IsMatch(password, passwordPattern);
     }
 
     public string HashPassword(string password)
     {
-        using (var sha256Hash = SHA256.Create())
+        using (SHA256 sha256Hash = SHA256.Create())
         {
-            var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
-            var builder = new StringBuilder();
-            foreach (var b in
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in
                      bytes) builder.Append(b.ToString("x2"));
 
             return builder.ToString();
@@ -32,7 +32,7 @@ public class PasswordManager : IPasswordManager
 
     public bool VerifyPassword(string plainPassword, string storedHash)
     {
-        var hashedPassword = HashPassword(plainPassword);
+        string hashedPassword = HashPassword(plainPassword);
         return hashedPassword == storedHash;
     }
 }
