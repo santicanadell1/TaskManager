f41339f1c9e53a55820af7722bdffbbbb27f2759
nbs282 <Norapala462@gmail.com>
2025-06-11 20:19:13 -0300

    advance in leader project controller

---
+++ Controllers/LeaderProjectController.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using Service;
+using Service.Interface;
+using Service.Models;
+
+namespace Controllers;
+
+public class LeaderProjectController
+{
+    private readonly ILeaderPService _LeaderPService;
+
+    public LeaderProjectController(IRepositoryManager repositoryManager)
+    {
+        _LeaderPService = new LeaderPService(repositoryManager);
+    }
+
+    public void AssignMembersToProject(string projectName, List<UserDTO> members)
+    {
+        _LeaderPService.
+    }
+}
