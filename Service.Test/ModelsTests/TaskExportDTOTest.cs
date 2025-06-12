e3f91de4a8906f43213dc46fc95e7fb8151ac36b
Santi <santicanadell0@gmail.com>
2025-06-12 17:41:46 -0300

    Red: TaskExportDTO not initialized

---
+++ Service.Test/ModelsTests/TaskExportDTOTest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Service.Models;
+
+namespace Service.Test.ModelsTests;
+
+[TestClass]
+public class TaskExportDTOTest
+{
+    [TestMethod]
+    public void TaskExportDTO_ShouldSetTaskProperty()
+    {
+        var taskExportDTO = new TaskExportDTO();
+        taskExportDTO.Task = "Test Task";
+        
+        Assert.AreEqual("Test Task", taskExportDTO.Task);
+    }
+
+    
+}
--- Service/Exporter/CSVExporter.cs
+++ Service/Exporter/CSVExporter.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using System.Text;
+using DataAccess;
+using Service;
+using Service.Models;
+
+public class CSVExporter : ExporterBase
+{
+    private readonly TaskService _taskService;
+
+    public CSVExporter(IRepositoryManager repositoryManager)
+    {
+        _taskService = new TaskService(repositoryManager, new CpmService());
+    }
+
+    protected override string ExportData(List<ProjectDTO> projects)
+    {
+        var strings = new StringBuilder();
+        strings.AppendLine("Proyecto, Fecha de Inicio, Tarea, Fecha de Inicio, Duración, Crítico, Recursos");
+
+        //Fecha de Inicio (creciente)
+        foreach (var project in projects.OrderBy(p => p.StartDate))
+        {
+            List<TaskDTO> tasks = _taskService.GetTasks(project.Name);
+
+            // tareas por Título (decreciente)
+            foreach (var task in tasks.OrderByDescending(t => t.Title))
+            {
+                // recursos asociados a la tarea
+                var recursos = string.Join(",", task.Resources.Select(r => r.Name));
+
+                strings.AppendLine($"{project.Name},{project.StartDate:dd/MM/yyyy},{task.Title},{task.StartDate:dd/MM/yyyy},{task.Duration},{(task.IsCritical ? "S" : "N")},{recursos}");
+            }
+        }
+
+        return strings.ToString();
+    }
+}
