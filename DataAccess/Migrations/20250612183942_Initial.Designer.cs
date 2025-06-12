29064e4b23c349655c1b944a6fa8f9880dcb7a6e
nbs282 <Norapala462@gmail.com>
2025-06-12 15:44:49 -0300

    change the reset to default password to show password, and now change password when the user click on the button change password

--- DataAccess/Migrations/20250612183942_Initial.Designer.cs
+++ DataAccess/Migrations/20250612183942_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250611205430_Initial")]
+    [Migration("20250612183942_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
