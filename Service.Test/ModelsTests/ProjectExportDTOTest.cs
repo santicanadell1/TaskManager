0c84ccdc6668d699c944d219d3f846b106791dff
Santi <santicanadell0@gmail.com>
2025-06-12 17:50:14 -0300

    Red: ProjectExportDTO not initialized

---
+++ Service.Test/ModelsTests/ProjectExportDTOTest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Service.Models;
+
+namespace Service.Test.ModelsTests;
+
+[TestClass]
+public class ProjectExportDTOTest
+{
+    [TestMethod]
+    public void ProjectExportDTO_ShouldSetProjectProperty()
+    {
+        var projectExportDTO = new ProjectExportDTO
+        {
+            Project = "Test Project"
+        };
+
+        Assert.AreEqual("Test Project", projectExportDTO.Project);
+    }
+
+}
