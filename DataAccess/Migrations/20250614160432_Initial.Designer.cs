9a2909ec4c8d4cee7ad96115fd15841aa3bcba41
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-14 13:16:09 -0300

    update develop

--- DataAccess/Migrations/20250614160432_Initial.Designer.cs
+++ DataAccess/Migrations/20250614160432_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250614141224_Initial")]
+    [Migration("20250614160432_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
