26ada557e0efa24b16dfc354febd626eb24471ec
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-16 20:58:17 -0300

    Refactor AdminProjectController

--- Controllers/AdminProjectController.cs
+++ Controllers/AdminProjectController.cs
@@ -12,10 +12,60 @@ public class AdminProjectController
     {
         _adminPService = new AdminPService(repositoryManager);
     }
+    public void CreateProject(ProjectDTO project)
+    {
+        _adminPService.CreateProject(project);
+    }
+    public void RemoveProject(string projectName)
+    {
+        _adminPService.RemoveProject(projectName);
+    }
+    public void UpdateProject(string projectNameToUpdate, ProjectDTO project)
+    {
+        _adminPService.UpdateProject(projectNameToUpdate, project);
+    }
+    public ProjectDTO GetProject(string projectName)
+    {
+        return _adminPService.GetProjectByName(projectName);
+    }
+    
+    
+    public void AssignMembersToProject(string projectName, List<UserDTO> usersToAssign)
+    {
+        _adminPService.AssignMembersToProject(projectName, usersToAssign);
+    }
+    public void RemoveMemberFromProject(string projectName, string userToRemoveEmail)
+    {
+        _adminPService.RemoveMemberFromProject(projectName, userToRemoveEmail);
+    }
+    public void AddTaskToMember(string projectName, string userToAddTaskEmail, string taskName)
+    {
+        _adminPService.AddTaskToMember(projectName, userToAddTaskEmail, taskName);
+    }
+    public void RemoveTaskFromMember(string projectName, string userToDeleteTaskEmail, string taskName)
+    {
+        _adminPService.RemoveTaskFromMember(projectName, userToDeleteTaskEmail, taskName);
+    }
+    public void SetProjectLeaderToProject(string projectName, string usersToAssignLeaderEmail)
+    {
+        _adminPService.SetProjectLeader(projectName, usersToAssignLeaderEmail);
+    }
+    public void RemoveProjectLeaderFromProject(string projectName)
+    {
+        _adminPService.RemoveProjectLeader(projectName);
+    }
 
+    public List<ProjectDTO> GetAllProjects()
+    {
+        return _adminPService.GetAllProjects();
+    }
     public List<ProjectDTO> GetAllProjectsForUser(string userEmail)
     {
         return _adminPService.GetAllProjectsForUser(userEmail);
+    }
+    public List<ProjectDTO> GetAllProjectsForAdmin()
+    {
+        return _adminPService.GetAllProjectsForAdmin();
     }
 
     public List<TaskDTO> GetAllTaskForAMemberInAProject(string projectName, string userEmail)
@@ -23,73 +73,13 @@ public class AdminProjectController
         return _adminPService.GetAllTaskForAMemberInAProject(projectName, userEmail);
     }
 
-    public void CreateProject(ProjectDTO project)
-    {
-        _adminPService.CreateProject(project);
-    }
-
-    public List<ProjectDTO> GetAllProjects()
-    {
-        return _adminPService.GetAllProjects();
-    }
-
-    public void AssignMembersToProject(string projectName, List<UserDTO> usersToAssign)
-    {
-        _adminPService.AssignMembersToProject(projectName, usersToAssign);
-    }
-
-    public List<UserDTO> GetAllMembersOfAProject(string projectName)
-    {
-        return _adminPService.GetMembers(projectName);
-    }
-
-    public void RemoveProject(string projectName)
-    {
-        _adminPService.RemoveProject(projectName);
-    }
-
-    public void RemoveMemberFromProject(string projectName, string userToRemoveEmail)
-    {
-        _adminPService.RemoveMemberFromProject(projectName, userToRemoveEmail);
-    }
-
-    public void AddTaskToMember(string projectName, string userToAddTaskEmail, string taskName)
-    {
-        _adminPService.AddTaskToMember(projectName, userToAddTaskEmail, taskName);
-    }
-
-    public void RemoveTaskFromMember(string projectName, string userToDeleteTaskEmail, string taskName)
-    {
-        _adminPService.RemoveTaskFromMember(projectName, userToDeleteTaskEmail, taskName);
-    }
-
-    public void UpdateProject(string projectNameToUpdate, ProjectDTO project)
-    {
-        _adminPService.UpdateProject(projectNameToUpdate, project);
-    }
-
     public List<UserDTO> GetAllProjectLeaderUsers()
     {
         return _adminPService.GetAllProjectLeaderUsers();
     }
-
-    public void SetProjectLeaderToProject(string projectName, string usersToAssignLeaderEmail)
+    public List<UserDTO> GetAllMembersOfAProject(string projectName)
     {
-        _adminPService.SetProjectLeader(projectName, usersToAssignLeaderEmail);
+        return _adminPService.GetMembers(projectName);
     }
-
-    public void RemoveProjectLeaderFromProject(string projectName)
-    {
-        _adminPService.RemoveProjectLeader(projectName);
-    }
-
-    public ProjectDTO GetProject(string projectName)
-    {
-        return _adminPService.GetProjectByName(projectName);
-    }
-
-    public List<ProjectDTO> GetAllProjectsForAdmin()
-    {
-        return _adminPService.GetAllProjectsForAdmin();
-    }
+    
 }
