8539a70cf8c8da7f9f04b23d5764759b43f3dea3
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-17 20:54:40 -0300

    Refactor: removing vars in ProjectDTOTest

--- Service.Test/ModelsTests/ProjectDTOTest.cs
+++ Service.Test/ModelsTests/ProjectDTOTest.cs
@@ -1,150 +1,169 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Service.Models;
 
-namespace Service.Test.ModelsTests;
-
-[TestClass]
-public class ProjectDTOTests
+namespace Service.Test.ModelsTests
 {
-    private UserDTO Admin;
-
-    private UserDTO Leader;
-
-    private List<UserDTO> members;
-
-    private UserDTO User;
-
-    [TestInitialize]
-    public void Initialize()
+    [TestClass]
+    public class ProjectDTOTests
     {
-        Admin = new UserDTO
+        private UserDTO Admin;
+        private UserDTO Leader;
+        private List<UserDTO> members;
+        private UserDTO User;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            FirstName = "Admin",
-            LastName = "User",
-            Email = "admin.user@example.com",
-            Birthday = DateTime.Parse("1990-01-01"),
-            Password = "Password123@",
-            Roles = new List<RolDTO> { RolDTO.AdminProject }
-        };
-        User = new UserDTO
+            Admin = new UserDTO
+            {
+                FirstName = "Admin",
+                LastName = "User",
+                Email = "admin.user@example.com",
+                Birthday = DateTime.Parse("1990-01-01"),
+                Password = "Password123@",
+                Roles = new List<RolDTO> { RolDTO.AdminProject }
+            };
+            User = new UserDTO
+            {
+                FirstName = "User",
+                LastName = "Member",
+                Email = "member.user@example.com",
+                Birthday = DateTime.Parse("1990-01-01"),
+                Password = "Password123@",
+                Roles = new List<RolDTO> { RolDTO.ProjectMember }
+            };
+            Leader = new UserDTO
+            {
+                FirstName = "Leader",
+                LastName = "Member",
+                Email = "leader.user@example.com",
+                Birthday = DateTime.Parse("1985-05-01"),
+                Password = "Password123@",
+                Roles = new List<RolDTO> { RolDTO.ProjectMember }
+            };
+            members = new List<UserDTO> { User };
+        }
+
+        [TestMethod]
+        public void Validate_ShouldThrowValidationException_WhenNameIsNullOrWhiteSpace()
         {
-            FirstName = "User",
-            LastName = "Member",
-            Email = "member.user@example.com",
-            Birthday = DateTime.Parse("1990-01-01"),
-            Password = "Password123@",
-            Roles = new List<RolDTO> { RolDTO.ProjectMember }
-        };
-        Leader = new UserDTO
+            ProjectDTO projectDTO = new ProjectDTO
+            {
+                Name = "",
+                Description = "Test Description",
+                StartDate = DateTime.Now,
+                AdminProyect = Admin,
+                Members = members,
+                ProjectLeader = Leader
+            };
+
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(
+                projectDTO,
+                new ValidationContext(projectDTO),
+                validationResults,
+                true
+            );
+
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldThrowValidationException_WhenDescriptionIsNullOrWhiteSpace()
         {
-            FirstName = "Leader",
-            LastName = "Member",
-            Email = "leader.user@example.com",
-            Birthday = DateTime.Parse("1985-05-01"),
-            Password = "Password123@",
-            Roles = new List<RolDTO> { RolDTO.ProjectMember }
-        };
-        members = new List<UserDTO> { User };
-    }
+            ProjectDTO projectDTO = new ProjectDTO
+            {
+                Name = "Test Project",
+                Description = "",
+                StartDate = DateTime.Now,
+                AdminProyect = Admin,
+                Members = members,
+                ProjectLeader = Leader
+            };
 
-    [TestMethod]
-    public void Validate_ShouldThrowValidationException_WhenNameIsNullOrWhiteSpace()
-    {
-        var projectDTO = new ProjectDTO
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(
+                projectDTO,
+                new ValidationContext(projectDTO),
+                validationResults,
+                true
+            );
+
+            Assert.IsFalse(isValid);
+        }
+
+        [TestMethod]
+        public void Validate_ShouldThrowValidationException_WhenLeaderIsNull()
         {
-            Name = "",
-            Description = "Test Description",
-            StartDate = DateTime.Now,
-            AdminProyect = Admin,
-            Members = members,
-            ProjectLeader = Leader
-        };
+            ProjectDTO projectDTO = new ProjectDTO
+            {
+                Name = "Test Project",
+                Description = "Test Description",
+                StartDate = DateTime.Now,
+                AdminProyect = Admin,
+                Members = members,
+                ProjectLeader = null
+            };
 
-        var validationResults = new List<ValidationResult>();
-        var isValid =
-            Validator.TryValidateObject(projectDTO, new ValidationContext(projectDTO), validationResults, true);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(
+                projectDTO,
+                new ValidationContext(projectDTO),
+                validationResults,
+                true
+            );
 
-        Assert.IsFalse(isValid);
-    }
+            Assert.IsTrue(isValid);
+        }
 
-    [TestMethod]
-    public void Validate_ShouldThrowValidationException_WhenDescriptionIsNullOrWhiteSpace()
-    {
-        var projectDTO = new ProjectDTO
+        [TestMethod]
+        public void Validate_ShouldNotThrowValidationException_WhenAllFieldsAreValid()
         {
-            Name = "Test Project",
-            Description = "",
-            StartDate = DateTime.Now,
-            AdminProyect = Admin,
-            Members = members,
-            ProjectLeader = Leader
-        };
+            ProjectDTO projectDTO = new ProjectDTO
+            {
+                Name = "Test Project",
+                Description = "Test Description",
+                StartDate = DateTime.Parse("2020-09-01"),
+                AdminProyect = Admin,
+                Members = members,
+                ProjectLeader = Leader
+            };
 
-        var validationResults = new List<ValidationResult>();
-        var isValid =
-            Validator.TryValidateObject(projectDTO, new ValidationContext(projectDTO), validationResults, true);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(
+                projectDTO,
+                new ValidationContext(projectDTO),
+                validationResults,
+                true
+            );
 
-        Assert.IsFalse(isValid);
-    }
+            Assert.IsTrue(isValid);
+        }
 
-    [TestMethod]
-    public void Validate_ShouldThrowValidationException_WhenLeaderIsNull()
-    {
-        var projectDTO = new ProjectDTO
+        [TestMethod]
+        public void Validate_AtributesMustBeValid()
         {
-            Name = "Test Project",
-            Description = "Test Description",
-            StartDate = DateTime.Now,
-            AdminProyect = Admin,
-            Members = members,
-            ProjectLeader = null
-        };
+            ProjectDTO projectDTO = new ProjectDTO
+            {
+                Name = "Test Project",
+                Description = "Test Description",
+                StartDate = DateTime.Parse("2020-09-01"),
+                AdminProyect = Admin,
+                Members = members,
+                ProjectLeader = Leader
+            };
 
-        var validationResults = new List<ValidationResult>();
-        var isValid =
-            Validator.TryValidateObject(projectDTO, new ValidationContext(projectDTO), validationResults, true);
-
-        Assert.IsTrue(isValid);
-    }
-
-    [TestMethod]
-    public void Validate_ShouldNotThrowValidationException_WhenAllFieldsAreValid()
-    {
-        var projectDTO = new ProjectDTO
-        {
-            Name = "Test Project",
-            Description = "Test Description",
-            StartDate = DateTime.Parse("2020-09-01"),
-            AdminProyect = Admin,
-            Members = members,
-            ProjectLeader = Leader
-        };
-
-        var validationResults = new List<ValidationResult>();
-        var isValid =
-            Validator.TryValidateObject(projectDTO, new ValidationContext(projectDTO), validationResults, true);
-
-        Assert.IsTrue(isValid);
-    }
-
-    [TestMethod]
-    public void Validate_AtributesMustBeValid()
-    {
-        var projectDTO = new ProjectDTO
-        {
-            Name = "Test Project",
-            Description = "Test Description",
-            StartDate = DateTime.Parse("2020-09-01"),
-            AdminProyect = Admin,
-            Members = members,
-            ProjectLeader = Leader
-        };
-
-        Assert.IsTrue(projectDTO.StartDate == DateTime.Parse("2020-09-01") &&
-                      projectDTO.Name == "Test Project" &&
-                      projectDTO.Description == "Test Description" &&
-                      projectDTO.AdminProyect == Admin &&
-                      projectDTO.Members == members &&
-                      projectDTO.ProjectLeader == Leader);
+            Assert.IsTrue(
+                projectDTO.StartDate == DateTime.Parse("2020-09-01") &&
+                projectDTO.Name == "Test Project" &&
+                projectDTO.Description == "Test Description" &&
+                projectDTO.AdminProyect == Admin &&
+                projectDTO.Members == members &&
+                projectDTO.ProjectLeader == Leader
+            );
+        }
     }
 }
