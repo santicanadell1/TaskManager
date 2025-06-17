7abe9fafff48bc511539c0e389c0b23fbe2bbc9a
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-17 20:59:35 -0300

    Refactor: removing vars in TaskExportDTOTest

--- Service.Test/ModelsTests/TaskExportDTOTest.cs
+++ Service.Test/ModelsTests/TaskExportDTOTest.cs
@@ -1,52 +1,59 @@
-﻿namespace Service.Test.ModelsTests;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Models;
 
-[TestClass]
-public class TaskExportDTOTest
+namespace Service.Test.ModelsTests
 {
-    [TestMethod]
-    public void TaskExportDTO_ShouldSetTaskProperty()
+    [TestClass]
+    public class TaskExportDTOTest
     {
-        var taskExportDTO = new TaskExportDTO();
-        taskExportDTO.Task = "Test Task";
+        [TestMethod]
+        public void TaskExportDTO_ShouldSetTaskProperty()
+        {
+            TaskExportDTO taskExportDTO = new TaskExportDTO();
+            taskExportDTO.Task = "Test Task";
 
-        Assert.AreEqual("Test Task", taskExportDTO.Task);
-    }
+            Assert.AreEqual("Test Task", taskExportDTO.Task);
+        }
 
-    [TestMethod]
-    public void TaskExportDTO_ShouldSetStartDateProperty()
-    {
-        var taskExportDTO = new TaskExportDTO();
-        taskExportDTO.StartDate = "2024-01-01";
+        [TestMethod]
+        public void TaskExportDTO_ShouldSetStartDateProperty()
+        {
+            TaskExportDTO taskExportDTO = new TaskExportDTO();
+            taskExportDTO.StartDate = "2024-01-01";
 
-        Assert.AreEqual("2024-01-01", taskExportDTO.StartDate);
-    }
+            Assert.AreEqual("2024-01-01", taskExportDTO.StartDate);
+        }
 
-    [TestMethod]
-    public void TaskExportDTO_ShouldSetDurationProperty()
-    {
-        var taskExportDTO = new TaskExportDTO();
-        taskExportDTO.Duration = 5;
+        [TestMethod]
+        public void TaskExportDTO_ShouldSetDurationProperty()
+        {
+            TaskExportDTO taskExportDTO = new TaskExportDTO();
+            int duration = 5;
+            taskExportDTO.Duration = duration;
 
-        Assert.AreEqual(5, taskExportDTO.Duration);
-    }
+            Assert.AreEqual(5, taskExportDTO.Duration);
+        }
 
-    [TestMethod]
-    public void TaskExportDTO_ShouldSetIsCriticalProperty()
-    {
-        var taskExportDTO = new TaskExportDTO();
-        taskExportDTO.IsCritical = "Yes";
+        [TestMethod]
+        public void TaskExportDTO_ShouldSetIsCriticalProperty()
+        {
+            TaskExportDTO taskExportDTO = new TaskExportDTO();
+            string isCritical = "Yes";
+            taskExportDTO.IsCritical = isCritical;
 
-        Assert.AreEqual("Yes", taskExportDTO.IsCritical);
-    }
+            Assert.AreEqual("Yes", taskExportDTO.IsCritical);
+        }
 
-    [TestMethod]
-    public void TaskExportDTO_ShouldSetResourcesProperty()
-    {
-        var taskExportDTO = new TaskExportDTO();
-        var resources = new List<string> { "Resource1", "Resource2" };
-        taskExportDTO.Resources = resources;
+        [TestMethod]
+        public void TaskExportDTO_ShouldSetResourcesProperty()
+        {
+            TaskExportDTO taskExportDTO = new TaskExportDTO();
+            List<string> resources = new List<string> { "Resource1", "Resource2" };
+            taskExportDTO.Resources = resources;
 
-        Assert.AreEqual(2, taskExportDTO.Resources.Count);
-        Assert.AreEqual("Resource1", taskExportDTO.Resources[0]);
+            Assert.AreEqual(2, taskExportDTO.Resources.Count);
+            Assert.AreEqual("Resource1", taskExportDTO.Resources[0]);
+        }
     }
 }
