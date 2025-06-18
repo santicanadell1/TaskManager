dff75f487f36a2f935371c22d69844742a986d89
Santi <santicanadell0@gmail.com>
2025-06-18 13:34:33 -0300

    Creation of IProjectRepository

---
+++ DataAccess/Repositories/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace DataAccess;
+
+public class IProjectRepository
+{
+    public interface IRepository<T>
+    {
+        public List<T> GetAll();
+        public T Get(Func<T, bool> filter);
+        public void Add(T entity);
+        public void Update(T entity);
+        public void Delete(T entity);
+    }
+}
