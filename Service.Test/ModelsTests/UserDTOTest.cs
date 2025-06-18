1b336101d550896110f35a5412bc92a69f8f7600
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-17 21:00:20 -0300

    Refactor: removing vars in UserDTOTest

--- Service.Test/ModelsTests/UserDTOTest.cs
+++ Service.Test/ModelsTests/UserDTOTest.cs
@@ -1,67 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Service.Models;
 
-namespace Service.Test.ModelsTests;
-
-[TestClass]
-public class UserDTOTest
+namespace Service.Test.ModelsTests
 {
-    [TestMethod]
-    public void NewUser_WhenFirstNameIsNull_ThenUserIsNotCreated()
+    [TestClass]
+    public class UserDTOTest
     {
-        var user = new UserDTO { FirstName = null };
+        [TestMethod]
+        public void NewUser_WhenFirstNameIsNull_ThenUserIsNotCreated()
+        {
+            UserDTO user = new UserDTO { FirstName = null };
+            Assert.IsNull(user.FirstName);
+        }
 
-        Assert.IsNull(user.FirstName);
-    }
+        [TestMethod]
+        public void NewUser_WhenLastNameIsNull_ThenUserIsNotCreated()
+        {
+            UserDTO user = new UserDTO { LastName = null };
+            Assert.IsNull(user.LastName);
+        }
 
-    [TestMethod]
-    public void NewUser_WhenLastNameIsNull_ThenUserIsNotCreated()
-    {
-        var user = new UserDTO { LastName = null };
+        [TestMethod]
+        public void NewUser_WhenEmailIsNull_ThenUserIsNotCreated()
+        {
+            UserDTO user = new UserDTO { Email = null };
+            Assert.IsNull(user.Email);
+        }
 
-        Assert.IsNull(user.LastName);
-    }
+        [TestMethod]
+        public void NewUser_WhenBirthdayIsDefault_ThenUserIsNotCreated()
+        {
+            UserDTO user = new UserDTO { Birthday = default(DateTime) };
+            Assert.AreEqual(default(DateTime), user.Birthday);
+        }
 
-    [TestMethod]
-    public void NewUser_WhenEmailIsNull_ThenUserIsNotCreated()
-    {
-        var user = new UserDTO { Email = null };
+        [TestMethod]
+        public void NewUser_WhenPasswordIsNull_ThenUserIsNotCreated()
+        {
+            UserDTO user = new UserDTO { Password = null };
+            Assert.IsNull(user.Password);
+        }
 
-        Assert.IsNull(user.Email);
-    }
+        [TestMethod]
+        public void NewUser_WhenRolesAreAssigned_ThenRolesAreSet()
+        {
+            List<RolDTO> roles = new List<RolDTO> { RolDTO.AdminSystem, RolDTO.ProjectMember };
+            UserDTO user = new UserDTO { Roles = roles };
 
-    [TestMethod]
-    public void NewUser_WhenBirthdayIsDefault_ThenUserIsNotCreated()
-    {
-        var user = new UserDTO { Birthday = default };
+            Assert.IsNotNull(user.Roles);
+            Assert.AreEqual(2, user.Roles.Count);
+            Assert.IsTrue(user.Roles.Contains(RolDTO.AdminSystem));
+            Assert.IsTrue(user.Roles.Contains(RolDTO.ProjectMember));
+        }
 
-        Assert.AreEqual(default, user.Birthday);
-    }
+        [TestMethod]
+        public void NewUser_WhenTasksAreAssigned_ThenTasksAreSet()
+        {
+            List<TaskDTO> tasks = new List<TaskDTO>
+            {
+                new TaskDTO { Title = "Task1" }
+            };
+            UserDTO user = new UserDTO { Tasks = tasks };
 
-    [TestMethod]
-    public void NewUser_WhenPasswordIsNull_ThenUserIsNotCreated()
-    {
-        var user = new UserDTO { Password = null };
-
-        Assert.IsNull(user.Password);
-    }
-
-    [TestMethod]
-    public void NewUser_WhenRolesAreAssigned_ThenRolesAreSet()
-    {
-        var roles = new List<RolDTO> { RolDTO.AdminSystem, RolDTO.ProjectMember };
-        var user = new UserDTO { Roles = roles };
-
-        Assert.IsNotNull(user.Roles);
-        Assert.AreEqual(2, user.Roles.Count);
-        Assert.IsTrue(user.Roles.Contains(RolDTO.AdminSystem));
-        Assert.IsTrue(user.Roles.Contains(RolDTO.ProjectMember));
-    }
-
-    [TestMethod]
-    public void NewUser_WhenTasksAreAssigned_ThenTasksAreSet()
-    {
-        var tasks = new List<TaskDTO> { new() { Title = "Task1" } };
-        var user = new UserDTO { Tasks = tasks };
-        Assert.IsNotNull(user.Tasks);
+            Assert.IsNotNull(user.Tasks);
+        }
     }
 }
