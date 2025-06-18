d6f2b6d3978efbc023398fcfe4b329f3473fe84b
Feli1035 <FD281987@fi365.ort.edu.uy>
2025-06-17 21:41:35 -0300

    Refactor: removing vars in PasswordMServiceTest

--- Service.Test/PasswordMTest.cs
+++ Service.Test/PasswordMTest.cs
@@ -6,9 +6,9 @@ public class PasswordMTest
     [TestMethod]
     public void DefaultPassword_ShouldBeValid()
     {
-        var passwordManager = new PasswordManager();
+        PasswordManager passwordManager = new PasswordManager();
 
-        var isValid = passwordManager.IsValidPassword(passwordManager.getDefaultPassword());
+        bool isValid = passwordManager.IsValidPassword(passwordManager.getDefaultPassword());
 
         Assert.IsTrue(isValid, "The default password should be valid.");
     }
@@ -16,12 +16,12 @@ public class PasswordMTest
     [TestMethod]
     public void HashPassword_ShouldReturnSameHashForSamePassword()
     {
-        var passwordManager = new PasswordManager();
-        var password1 = "FNSabc1?";
-        var password2 = "FNSabc1?";
+        PasswordManager passwordManager = new PasswordManager();
+        string password1 = "FNSabc1?";
+        string password2 = "FNSabc1?";
 
-        var hash1 = passwordManager.HashPassword(password1);
-        var hash2 = passwordManager.HashPassword(password2);
+        string hash1 = passwordManager.HashPassword(password1);
+        string hash2 = passwordManager.HashPassword(password2);
 
         Assert.AreEqual(hash1, hash2, "The hash should be the same for the same password.");
     }
@@ -29,12 +29,12 @@ public class PasswordMTest
     [TestMethod]
     public void VerifyPassword_ShouldReturnFalse_WhenPasswordDoesNotMatchHash()
     {
-        var passwordManager = new PasswordManager();
-        var originalPassword = "FNSabc1?";
-        var wrongPassword = "Incorrect1?";
-        var hashedPassword = passwordManager.HashPassword(originalPassword);
+        PasswordManager passwordManager = new PasswordManager();
+        string originalPassword = "FNSabc1?";
+        string wrongPassword = "Incorrect1?";
+        string hashedPassword = passwordManager.HashPassword(originalPassword);
 
-        var isPasswordValid = passwordManager.VerifyPassword(wrongPassword, hashedPassword);
+        bool isPasswordValid = passwordManager.VerifyPassword(wrongPassword, hashedPassword);
 
         Assert.IsFalse(isPasswordValid, "The wrong password should not match the hash.");
     }
